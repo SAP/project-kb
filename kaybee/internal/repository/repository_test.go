263785616573cf423ec62d92a98b9bf0b69eb830
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2020-09-02 11:05:37 +0200

    chore: disable test that depends on github authentication to succeed

--- kaybee/internal/repository/repository_test.go
+++ kaybee/internal/repository/repository_test.go
@@ -1,34 +1,28 @@
 package repository
 
-import (
-	"log"
-	"os"
-	"testing"
-)
+// func TestFetch(t *testing.T) {
+// 	repository := NewRepository("https://github.com/ichbinfrog/test_2", "master", true, 1, "file:///../../.kaybee/repositories")
 
-func TestFetch(t *testing.T) {
-	repository := NewRepository("https://github.com/ichbinfrog/test_2", "master", true, 1, "file:///../../.kaybee/repositories")
+// 	// defer os.RemoveAll(repository.Path)
 
-	// defer os.RemoveAll(repository.Path)
+// 	// Remove old repository
+// 	os.RemoveAll(repository.Path)
 
-	// Remove old repository
-	os.RemoveAll(repository.Path)
+// 	// Test repository fetching from empty repo
+// 	repository.Fetch(false)
 
-	// Test repository fetching from empty repo
-	repository.Fetch(false)
+// 	// Test repository fetching from preexisting repo
+// 	repository.Fetch(false)
 
-	// Test repository fetching from preexisting repo
-	repository.Fetch(false)
-
-	// Test metadata fill
-	statements, err := repository.Statements()
-	if err != nil {
-		t.Error(err)
-	}
-	for _, s := range statements {
-		log.Printf("%+v\n", s)
-	}
-}
+// 	// Test metadata fill
+// 	statements, err := repository.Statements()
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	for _, s := range statements {
+// 		log.Printf("%+v\n", s)
+// 	}
+// }
 
 /* func TestListFetch(t *testing.T) {
 	repository := &List{}
