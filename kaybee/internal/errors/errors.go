428b5b71106ba0d8fa64838d6f46176a71262ad3
Quoc Trung Hoang <quoc-trung.hoang@etu.utc.fr>
2020-09-09 09:38:23 +0200

    Decouple errors from display
    Signed-off-by: Quoc Trung Hoang <quoc-trung.hoang@etu.utc.fr>

--- kaybee/internal/errors/errors.go
+++ kaybee/internal/errors/errors.go
@@ -10,47 +10,47 @@ import (
 
 var (
 	// ErrUntrustedContributor is thrown when a contributor is not trusted
-	ErrUntrustedContributor = errors.New(" ! Contributor not found in list of trusted contributors")
+	ErrUntrustedContributor = errors.New("Contributor not found in list of trusted contributors")
 
 	// ErrConflictingStatements is thrown when two staments are not independent and cannot be merged with the current policy
-	ErrConflictingStatements = errors.New(" ! Conflicting statements")
+	ErrConflictingStatements = errors.New("Conflicting statements")
 
 	// ErrNonIndependentStatements is thrown when two staments are not independent
-	ErrNonIndependentStatements = errors.New(" ! Non-independent statements")
+	ErrNonIndependentStatements = errors.New("Non-independent statements")
 
 	// ErrNothingToReconcile is thrown when two staments are independent and no reconciliation is needed
-	ErrNothingToReconcile = errors.New(" ! Statements are independent, nothing to reconcile")
+	ErrNothingToReconcile = errors.New("Statements are independent, nothing to reconcile")
 )
 
 var (
 	// ErrConfigBackendRequired is thrown when no backend is supplied in a configuration
-	ErrConfigBackendRequired = errors.New(" ! Backend is required in sync configuration")
+	ErrConfigBackendRequired = errors.New("Backend is required in sync configuration")
 
 	// ErrConfigValidationFailed is thrown when the validation of the configuration fails
-	ErrConfigValidationFailed = errors.New(" ! Sync configuration validation failed")
+	ErrConfigValidationFailed = errors.New("Sync configuration validation failed")
 
 	// ErrConfigVersionNotDefined is thrown when no API version is specified in a configuration
-	ErrConfigVersionNotDefined = errors.New(" ! ApiVersion not defined in configuration file")
+	ErrConfigVersionNotDefined = errors.New("ApiVersion not defined in configuration file")
 
 	// ErrConfigInvalidImportRegex is thrown when an invalid import regex is specified in a configuration
-	ErrConfigInvalidImportRegex = errors.New(" ! Invalid import regex")
+	ErrConfigInvalidImportRegex = errors.New("Invalid import regex")
 
 	// ErrConfigInvalidExportRegex is thrown when an invalid export regex is specified in a configuration
-	ErrConfigInvalidExportRegex = errors.New(" ! Invalid export regex")
+	ErrConfigInvalidExportRegex = errors.New("Invalid export regex")
 
 	// ErrConfigPolicyMissing is thrown when no policy is specified in a configuration
 	// TODO: rename to ErrConfigPolicyMissing, the choice of using a default needs not be
 	// hardcoded here, so the term 'default' should not be used
-	ErrConfigPolicyMissing = errors.New(" ! Policy not set")
+	ErrConfigPolicyMissing = errors.New("Policy not set")
 
 	// ErrConfigUnknownPolicy is thrown when an unknown/invalid policy is specified in a configuration
-	ErrConfigUnknownPolicy = errors.New(" ! Unknown policy listed in policies")
+	ErrConfigUnknownPolicy = errors.New("Unknown policy listed in policies")
 
 	// ErrConfigInvalidSourceURL is thrown when a source repository is specified without a valid URL
-	ErrConfigInvalidSourceURL = errors.New(" ! Source does not have a repo associated")
+	ErrConfigInvalidSourceURL = errors.New("Source does not have a repo associated")
 
 	// ErrConfigInvalid is used to signal any configuration error that is not covered by the other config errors
-	ErrConfigInvalid = errors.New(" ! Configuration is invalid")
+	ErrConfigInvalid = errors.New("Configuration is invalid")
 )
 
 // CheckErr verifies the type of error and choses to display a warning
