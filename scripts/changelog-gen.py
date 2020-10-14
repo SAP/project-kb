3a490a663e66bb6a3d417c3b997594f69dea41d7
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2020-10-14 16:55:51 +0200

    chore: improve changelog generation

--- scripts/changelog-gen.py
+++ scripts/changelog-gen.py
@@ -76,6 +76,15 @@ def render_changelog_header():
     print("<a name=\"" + curr_tag + "\"></a>")
     print("## [" + curr_tag + "](https://github.com/" + project_slug + "/compare/" + prev_tag + "..." + curr_tag +")")
 
+def render_link_to_detailed_changes():
+    since = get_commit_for_second_last_tag()
+    until = get_commit_for_last_tag()
+
+    prev_tag = tag_at_commit(since)
+    curr_tag = tag_at_commit(until)
+
+    print("\n\n:mag: [View detailed changes since the previous release]" + "(https://github.com/" + project_slug + "/compare/" + prev_tag + "..." + curr_tag +")")
+
 def render_changelog(log):
     changes_by_type = dict()
 
@@ -99,8 +108,9 @@ def main(args):
     else:
         log = get_commits()
 
-    render_changelog_header()
+ #   render_changelog_header()
     render_changelog(log)
+    render_link_to_detailed_changes()
 
 
 if __name__ == "__main__":
