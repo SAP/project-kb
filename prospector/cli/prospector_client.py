26e1666cd5b8e76691b84faf10007311756f59ab
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2021-04-03 11:25:33 +0200

    added missing changes from previous commit

--- prospector/cli/prospector_client.py
+++ prospector/cli/prospector_client.py
@@ -30,8 +30,18 @@ def prospector(
     repository = Git(repository, git_cache)
     repository.clone()
     tags = repository.get_tags()
-    print(tags)
+
+    if debug:
+        print("Found tags:")
+        print(tags)
+
     print("Done")
+
+    # STEP 1: filter based on commit size
+
+    # STEP 2: filter based on time
+
+    # STEP 3: filter based on file extensions
 
     # TODO take some code from legacy filter.py
 
@@ -40,3 +50,73 @@ def prospector(
 
     if debug:
         pprint(advisory_record)
+
+
+def select_commit_ids_based_on_vulnerability_publish_date(
+    vulnerability_published_timestamp,
+    git_repo=None,
+    repo_url=None,
+    days_before=730,
+    days_after=100,
+    commits_before_cap=5215,
+    commits_after_cap=100,
+):
+    """
+    To select commit IDs based on the vulnerability publish date.
+    This can be used as a starting position for the search for fix commits.
+
+    Input:
+        vulnerability_published_timestamp (int): the timestamp at which the vulnerability is been published i.e. in the NVD
+        git_repo (git_explorer.core.Git): to use for extracting the content
+        repository_url: if git_repo is not provided, a repository url is needed to initialize the git_repo
+        days_before (int): the maximum number of days before the release timestamp (edge)
+        days_after (int): the maximum number of days after the release timestamp (edge)
+        commits_before_cap (int): the maximum number of commits before the release timestamp (edge)
+        commits_after_cap (int): the maximum number of commits after the release timestamp (edge)
+
+    Returns:
+        list: a list of commit IDs within the interval
+    """
+
+    if git_repo == None:
+        try:
+            git_repo = Git(repo_url, cache_path=GIT_CACHE)
+            git_repo.clone(skip_existing=True)
+        except:
+            raise TypeError(
+                "git-repo should be of type git_explorer.core.Git, not {}, or repo_url should be a valid github repository url.".format(
+                    type(git_repo)
+                )
+            )
+
+    ### Add commits before NVD release
+    since, until = database.timestamp_to_timestamp_interval(
+        int(vulnerability_published_timestamp), days_before=days_before, days_after=0
+    )
+    commit_ids_to_add_before = database.get_commit_ids_between_timestamp_interval(
+        str(since), str(until), git_repo=git_repo, repository_url=repo_url
+    )
+
+    # maximum to add
+    if len(commit_ids_to_add_before) > commits_before_cap:
+        commit_ids_to_add_before = commit_ids_to_add_before[
+            :commits_before_cap
+        ]  # add the 5215 closest before the NVD release date
+
+    ### Add commits after NVD release
+    since, until = database.timestamp_to_timestamp_interval(
+        int(vulnerability_published_timestamp), days_before=0, days_after=days_after
+    )
+    commit_ids_to_add_after = database.get_commit_ids_between_timestamp_interval(
+        str(since), str(until), git_repo=git_repo, repository_url=repo_url
+    )
+
+    # maximum to add
+    if len(commit_ids_to_add_after) > commits_after_cap:
+        commit_ids_to_add_after = commit_ids_to_add_after[
+            -commits_after_cap:
+        ]  # add the 100 closest before the NVD release date
+
+    commit_ids = commit_ids_to_add_before + commit_ids_to_add_after
+
+    return commit_ids
