f0eeed05901e9c5a6d5e83fe18cd5d53b75ac940
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2021-04-14 01:24:39 +0200

    sketch of train algorithm

--- prospector/filter_rank/main.py
+++ prospector/filter_rank/main.py
@@ -20,8 +20,21 @@ def rank(adv_record: AdvisoryRecord, candidates: "list[Commit]") -> "list[Commit
     return candidates
 
 
-def train(model_name: str, dataset_file: str) -> bool:
+def train(model_name: str, data_filename: str) -> bool:
     """
     Takes in input a dataset and produces a trained model that is persisted to disk.
+    Dataset file contains triples:  (vuln_id,repository,commit_id)
     """
+    # read data_filename content in list of lines
+    # foreach l in line:
+    #   get (commit_id,repo) from l
+    #   if (commit_id,repo) not found in commidb:
+    #     commit_obj = make_commit(commit_id,repo)
+    #     preprocess(commit_obj)
+    #     save commit_obj to db
+    #   augment commit_obj with advisory-dependent features
+    #   add augmented commit_obj to dataframe
+    # fit(dataframe) --> model
+    # compute metrics
+    # save model to disk (file: model_name.bin)
     return True
