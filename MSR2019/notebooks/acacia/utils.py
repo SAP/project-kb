fa84a60c6e16348ba22b9a1019fe6411f4265a0d
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2021-04-19 10:29:53 +0200

    chore: reformatted code with black

--- MSR2019/notebooks/acacia/utils.py
+++ MSR2019/notebooks/acacia/utils.py
@@ -1,8 +1,11 @@
-import logging
-import re
-from datetime import datetime
+# flake8: noqa
+
+# import logging
+# import re
 import subprocess
-import traceback
+
+# import traceback
+from datetime import datetime
 
 # def execute_wait(cmd, cwd='/tmp', encoding='latin-1', timeout=None):
 #     logging.debug("exec: {} (in dir: {})".format( cmd, cwd ) )
@@ -15,7 +18,8 @@ import traceback
 #         logging.error(str(e))
 #         return False
 
-def execute(cmd, cwd='/tmp', encoding='latin-1'):
+
+def execute(cmd, cwd="/tmp", encoding="latin-1"):
     try:
         p2 = subprocess.Popen(cmd.split(), cwd=cwd, stdout=subprocess.PIPE)
         out, err = p2.communicate()
@@ -23,12 +27,13 @@ def execute(cmd, cwd='/tmp', encoding='latin-1'):
             # traceback.print_exc()
             return None
 
-        raw_output_list = out.decode(encoding).split('\n')
+        raw_output_list = out.decode(encoding).split("\n")
         return raw_output_list
 
     except Exception as e:
         # traceback.print_exc()
         return None
+
 
 # def flatten_lists(container):
 #     for i in container:
@@ -45,31 +50,33 @@ def execute(cmd, cwd='/tmp', encoding='latin-1'):
 #     string = re.sub(strip_special_chars, " ", string)
 #     return string.strip().split()
 
-class LatexExporter():
 
+class LatexExporter:
     def __init__(self):
         self.data = set()
 
     def __str__(self):
         self.print()
 
-    def save(self,k,v, comment=''):
-        print('[' + k + '] ' + comment.strip() + ': ' + str(v))
-        self.data.add((k,v,comment))
+    def save(self, k, v, comment=""):
+        print("[" + k + "] " + comment.strip() + ": " + str(v))
+        self.data.add((k, v, comment))
 
     def print(self):
         for d in self.data:
-            if d[2]!='':
-                print('\n% ' + str(d[2]))
-            print('\\newcommand{\\' + str(d[0]) +'}{' + str(d[1]) + '\\xspace}')
+            if d[2] != "":
+                print("\n% " + str(d[2]))
+            print("\\newcommand{\\" + str(d[0]) + "}{" + str(d[1]) + "\\xspace}")
 
     def to_file(self, filename):
-        with open(filename,'w') as f:
-            f.write('%\n% This file was auto-generated on ' + str(datetime.now()) +'\n%\n')
+        with open(filename, "w") as f:
+            f.write(
+                "%\n% This file was auto-generated on " + str(datetime.now()) + "\n%\n"
+            )
             for d in self.data:
-                line = ''
-                if d[2] != '':
-                    line += '\n% ' + str(d[2]) + '\n'
-                line += '\\newcommand{\\'+ d[0] + '}{' + str(d[1]) + '\\xspace}'
+                line = ""
+                if d[2] != "":
+                    line += "\n% " + str(d[2]) + "\n"
+                line += "\\newcommand{\\" + d[0] + "}{" + str(d[1]) + "\\xspace}"
                 print(line)
-                f.write(line + '\n')
+                f.write(line + "\n")
