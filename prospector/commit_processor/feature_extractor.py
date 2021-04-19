1368770438bc4e6fb7155681b2278c75ae644f5a
Péter Hegedűs <peter.h84@gmail.com>
2021-04-20 01:05:12 +0200

    Add feature "time_between_commit_and_advisory_record" (#112)
    Co-authored-by: idarav <idaravikusz@gmail.com>

--- prospector/commit_processor/feature_extractor.py
+++ prospector/commit_processor/feature_extractor.py
@@ -7,11 +7,22 @@ def extract_features(commit: Commit, advisory_record: AdvisoryRecord) -> CommitF
     references_vuln_id = extract_references_vuln_id(
         commit.cve_refs, advisory_record.vulnerability_id
     )
+    time_between_commit_and_advisory_record = extract_time_between_commit_and_advisory_record(
+        commit.timestamp, advisory_record.published_timestamp
+    )
     commit_feature = CommitFeatures(
-        commit=commit, references_vuln_id=references_vuln_id
+        commit=commit,
+        references_vuln_id=references_vuln_id,
+        time_between_commit_and_advisory_record=time_between_commit_and_advisory_record,
     )
     return commit_feature
 
 
 def extract_references_vuln_id(cve_references: "list[str]", cve_id: str) -> str:
     return cve_id in cve_references
+
+
+def extract_time_between_commit_and_advisory_record(
+    commit_timestamp: int, advisory_record_timestamp: int
+) -> int:
+    return commit_timestamp - advisory_record_timestamp
--- prospector/datamodel/commit_features_test.py
+++ prospector/datamodel/commit_features_test.py
@@ -8,7 +8,12 @@ def test_simple():
         repository="https://github.com/abc/xyz",
         timestamp="124234125",
     )
-    commit_features = CommitFeatures(commit=commit, references_vuln_id=True)
+    commit_features = CommitFeatures(
+        commit=commit,
+        references_vuln_id=True,
+        time_between_commit_and_advisory_record=42,
+    )
 
     assert commit_features.commit.repository == "https://github.com/abc/xyz"
     assert commit_features.references_vuln_id
+    assert commit_features.time_between_commit_and_advisory_record == 42
