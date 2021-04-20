68208765fd1a0c259a99c4bd6959bcb0d0d0ebdc
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2021-04-20 11:58:02 +0200

    chore: api testing (upload preproc. commits)

--- prospector/api/api_test.py
+++ prospector/api/api_test.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+from datamodel.commit import Commit
+
 
 from api.main import app
 
@@ -15,3 +17,11 @@ def test_status():
     response = client.get("/status")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+
+def test_post_preprocessed_commits():
+    commit_1 = Commit(repository="xxx", commit_id="yyy").__dict__
+    commit_2 = Commit(repository="aaa", commit_id="bbb").__dict__
+    commits = [commit_1, commit_2]
+    response = client.post("/commits/", json=commits)
+    assert response.status_code == 200
