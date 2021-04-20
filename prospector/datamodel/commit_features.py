72de4634eb42fceea6767e0ac2881f6df6c3765a
Péter Hegedűs <peter.h84@gmail.com>
2021-04-20 11:37:00 +0200

    Add changes_relevant_path feature (#111)
    Co-authored-by: idarav <idaravikusz@gmail.com>

--- prospector/datamodel/commit_features.py
+++ prospector/datamodel/commit_features.py
@@ -7,3 +7,4 @@ class CommitFeatures(BaseModel):
     commit: Commit
     references_vuln_id: bool = False
     time_between_commit_and_advisory_record: int = 0
+    changes_relevant_path: bool = False
--- prospector/datamodel/commit_features_test.py
+++ prospector/datamodel/commit_features_test.py
@@ -12,8 +12,10 @@ def test_simple():
         commit=commit,
         references_vuln_id=True,
         time_between_commit_and_advisory_record=42,
+        changes_relevant_path=True,
     )
 
     assert commit_features.commit.repository == "https://github.com/abc/xyz"
     assert commit_features.references_vuln_id
     assert commit_features.time_between_commit_and_advisory_record == 42
+    assert commit_features.changes_relevant_path
