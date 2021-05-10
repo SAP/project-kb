4e09505141ba6d4c264d2a968608e3a327e10be1
Ákos <akos.milankovich@search-lab.hu>
2021-05-10 12:43:05 +0200

    Merge branch 'upstream/prospector-assuremoss'

--- prospector/datamodel/commit_features_test.py
+++ prospector/datamodel/commit_features_test.py
@@ -13,6 +13,7 @@ def test_simple():
         references_vuln_id=True,
         time_between_commit_and_advisory_record=42,
         changes_relevant_path=True,
+        other_CVE_in_message=True,
         commit_falls_in_given_interval_based_on_advisory_publicatation_date=True,
         avg_hunk_size=10,
         n_hunks=12,
@@ -25,6 +26,7 @@ def test_simple():
     assert commit_features.references_vuln_id
     assert commit_features.time_between_commit_and_advisory_record == 42
     assert commit_features.changes_relevant_path
+    assert commit_features.other_CVE_in_message
     assert (
         commit_features.commit_falls_in_given_interval_based_on_advisory_publicatation_date
     )
