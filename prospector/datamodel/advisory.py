dd182a91cc535f9e050f035670ccc481bf73f420
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2021-05-21 19:18:17 +0200

    extract camelcase elements from advisory

--- prospector/datamodel/advisory.py
+++ prospector/datamodel/advisory.py
@@ -34,6 +34,7 @@ class AdvisoryRecord(BaseModel):
     from_nvd: bool = False
     nvd_rest_endpoint: str = NVD_REST_ENDPOINT
     paths: List[str] = Field(default_factory=list)
+    code_tokens: List[str] = Field(default_factory=list)
 
     def analyze(self, use_nvd: bool = False):
         self.from_nvd = use_nvd
@@ -46,6 +47,7 @@ class AdvisoryRecord(BaseModel):
         )
         self.affected_products = extract_products(self.description)
         self.paths = extract_path_tokens(self.description)
+        self.code_tokens = extract_camelcase_tokens(self.description)
 
     def _get_from_nvd(self, vuln_id: str, nvd_rest_endpoint: str = NVD_REST_ENDPOINT):
         """
@@ -79,47 +81,6 @@ class AdvisoryRecord(BaseModel):
         except Exception:
             print("Could not retrieve vulnerability data from NVD for " + vuln_id)
 
-    # """
-    # Information to provide when initializing an advisory record
-    # Input:
-    #     vulnerability_id (str): the vulnerability ID, typically a CVE
-    #     published_timestamp (str/int): the timestamp at which the vulnerability was
-    #                                    published, or patched if that is known
-    #     repo_url (str): the URL of the affected (GitHub) repository URL
-    #     nvd_references (list): references to which the NVD refers (1st level references)
-    #     references_content (str): the content that was extracted from these references,
-    #                               and will be used to compare lexical similarity with
-    #     advisory_references (list): the references that were extracted
-    #                                 from the NVD references (2nd level references)
-    #     vulnerability_description (str): the vulnerability description
-    #     connection (sqlite3.connection): the connection with the (commits) database
-    #     Optional:
-    #         preprocessed_vulnerability_description (str): if there is already
-    #                                       a preprocess vulnerability description
-    #         relevant_tags (list): a list of tags that are regarded as relevant tags
-    #                                (affected versions of the software)
-    #         verbose (bool): to print intermediate output
-    #         since (timestamp): lower bound for selecting the candidate commits
-    #         until (timestamp): upper bound for selecting the candidate commits
-    # """
-    # self.id = vulnerability_id
-    # self.published_timestamp = published_timestamp
-    # self.repo_url = re.sub("\.git$|/$", "", repo_url)
-    # self.is_github_url = "https://github.com" in repo_url
-    # self.project_name = extract_project_name_from_repository_url(self.repo_url)
-    # self.description = vulnerability_description
-    # self.relevant_tags = relevant_tags
-
-    # if preprocessed_vulnerability_description == None:
-    #     self.preprocessed_description = simpler_filter_text(
-    #         vulnerability_description
-    #     )
-    # else:
-    #     self.preprocessed_description = preprocessed_vulnerability_description
-
-    # # self.git_repo = Git(repo_url, cache_path=GIT_CACHE)
-    # # self.git_repo.clone(skip_existing=True)  # @TODO: true or false..?
-
 
 def extract_versions(text) -> "list[str]":
     """
@@ -139,6 +100,17 @@ def extract_products(text) -> "list[str]":
     regex = r"([A-Z]+[a-z\b]+)"
     result = list(set(re.findall(regex, text)))
     return [p for p in result if len(p) > 2]
+
+
+def extract_camelcase_tokens(text) -> "list[str]":
+    """
+    Extract camelcase or snake_case elements from advisory text
+    """
+    regex = r"([A-Za-z]+[a-z\b]+[A-Z]+[a-z\b]+)"
+    result = list(set(re.findall(regex, text)))
+    # return list(result)
+    return [p for p in result if len(p) > 2]
+    # return["blaHlafaHlsafs"]
 
 
 def extract_path_tokens(text: str) -> "list[str]":
