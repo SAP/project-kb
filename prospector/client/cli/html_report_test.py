f3d48184400c06599c118b2036582dfdf04747c3
Gergő Balogh <geryxyz@gmail.com>
2021-06-28 16:42:06 +0200

    basic title-based inner card layout with several fields and random commit generation

--- prospector/client/cli/html_report_test.py
+++ prospector/client/cli/html_report_test.py
@@ -78,7 +78,7 @@ def random_list_of_url(max_length: int, max_count: int):
 
 def random_list_of_cve(max_count: int):
     return [
-        f"CVE-{randint(10000, 99999)}-{randint(10, 99)}"
+        f"CVE-{randint(1987, 2021)}-{str(randint(10, 99)).rjust(4, '0')}"
         for _ in range(randint(0, max_count))
     ]
 
@@ -89,6 +89,27 @@ def random_commit_hash():
     return f"{hash:032x}"
 
 
+def random_hunk(start: int, stop: int):
+    start = randint(start, stop)
+    size = randint(start, stop)
+    return (start, start + size)
+
+
+def random_list_of_hunks(stop: int, max_count: int, start: int = 0):
+    return [random_hunk(start=start, stop=stop) for _ in range(randint(0, max_count))]
+
+
+def random_list_of_jira_refs(max_count: int):
+    return [
+        f"{choice(SAMPLES)}-{str(randint(0, 1000))}"
+        for _ in range(randint(0, max_count))
+    ]
+
+
+def random_list_of_github_issue_ids(stop: int, max_count: int, start: int = 0):
+    return [str(randint(start, stop)) for _ in range(randint(0, max_count))]
+
+
 def test_report_generation():
     candidates = []
     for _ in range(100):
@@ -97,16 +118,20 @@ def test_report_generation():
                 commit_id=random_commit_hash(),
                 repository=random_url(4),
                 message=" ".join(random_list_of_strs(100)),
+                timestamp=randint(0, 100000),
+                hunks=random_list_of_hunks(1000, 42),
+                diff=random_list_of_strs(200),
+                changed_files=random_list_of_path(4, 42),
+                message_reference_content=random_list_of_strs(42),
+                jira_refs=random_list_of_jira_refs(42),
+                ghissue_refs=random_list_of_github_issue_ids(100000, 42),
+                cve_refs=random_list_of_cve(42),
+                tags=random_list_of_strs(42),
             ),
             references_vuln_id=random_bool(),
-            references_ghissue=random_bool(),
             time_between_commit_and_advisory_record=randint(0, 42),
             changes_relevant_path=set(random_list_of_path(4, 42)),
             other_CVE_in_message=set(random_list_of_cve(42)),
-            avg_hunk_size=randint(0, 42),
-            n_hunks=randint(0, 42),
-            n_changed_files=randint(0, 42),
-            contains_jira_reference=random_bool(),
             referred_to_by_pages_linked_from_advisories=set(random_list_of_url(4, 42)),
             referred_to_by_nvd=set(random_list_of_url(4, 42)),
         )
--- prospector/datamodel/commit_features.py
+++ prospector/datamodel/commit_features.py
@@ -25,16 +25,20 @@ class CommitWithFeatures(BaseModel):
 
         self.n_hunks = self.commit.hunk_count
 
+        assert "avg_hunk_size" not in data
         if self.n_hunks > 0:
             hunk_lengths = [hunk[1] - hunk[0] for hunk in self.commit.hunks]
             self.avg_hunk_size = sum(hunk_lengths) / self.n_hunks
         else:
             self.avg_hunk_size = 0
 
+        assert "n_changed_files" not in data
         self.n_changed_files = len(self.commit.changed_files)
 
+        assert "references_ghissue" not in data
         self.references_ghissue = len(self.commit.ghissue_refs) > 0
 
+        assert "contains_jira_reference" not in data
         self.contains_jira_reference = len(self.commit.jira_refs) > 0
 
     def __hash__(self) -> int:
