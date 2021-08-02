d2038ad615ee6e78334d4db490d7b3accda07034
Gergő Balogh <geryxyz@gmail.com>
2021-08-02 16:54:14 +0200

    collection logic for stats

--- prospector/statistics/main.py
+++ prospector/statistics/main.py
@@ -9,17 +9,18 @@ LEVEL_DELIMITER = "."
 
 
 class StatisticCollection(dict):
-    def record(self, name: Union[str, Tuple[str, ...]], value):
+    def record(self, name: Union[str, Tuple[str, ...]], value, overwrite=False):
         if isinstance(name, str):
+            if not overwrite and name in self:
+                raise ForbiddenDuplication(f"{name} already added")
+
             if LEVEL_DELIMITER in name:
                 raise ValueError(
                     f"do not use level delimiter {LEVEL_DELIMITER} in names,"
                     " hierarchical entries should be specified by tuples"
                 )
-            elif name not in self:
-                self[name] = value
             else:
-                raise ForbiddenDuplication(f"{name} already added")
+                self[name] = value
         elif isinstance(name, tuple):
             if len(name) == 1:
                 self.record(name[0], value)
@@ -27,8 +28,69 @@ class StatisticCollection(dict):
                 current_name = name[0]
                 if current_name not in self:
                     self[current_name] = StatisticCollection()
-                    self[current_name].record(name[1:], value)
+                current_collection = self[current_name]
+                if not isinstance(current_collection, StatisticCollection):
+                    raise ForbiddenDuplication(
+                        f"{name} already added as a single value"
+                    )
                 else:
-                    raise ForbiddenDuplication(f"{name} already added")
+                    current_collection.record(name[1:], value)
         else:
             raise ValueError("only string or tuple keys are enabled")
+
+    def __getitem__(self, key: Union[str, Tuple[str, ...]]):
+        if isinstance(key, str):
+            return super().__getitem__(key)
+        elif isinstance(key, tuple):
+            if len(key) == 1:
+                return self[key[0]]
+            elif len(key) > 1:
+                current_collection = self[key[0]]
+                if isinstance(current_collection, StatisticCollection):
+                    return current_collection[key[1:]]
+                else:
+                    raise KeyError(f"{key[0]} is a single value")
+            else:
+                raise KeyError("empty key are not allowed")
+        else:
+            raise KeyError("only string ot tuple keys allowed")
+
+    def __contains__(self, key):
+        if isinstance(key, str):
+            return super().__contains__(key)
+        elif isinstance(key, tuple):
+            if len(key) == 1:
+                return key[0] in self
+            elif len(key) > 1:
+                if key[0] not in self:
+                    return False
+                current_collection = self[key[0]]
+                if isinstance(current_collection, StatisticCollection):
+                    return key[1:] in current_collection
+                else:
+                    raise KeyError(f"{key[0]} is a single value")
+            else:
+                raise KeyError("empty key are not allowed")
+        else:
+            raise KeyError("only string ot tuple keys allowed")
+
+    def collect(self, name: Union[str, Tuple[str, ...]], value):
+        if name not in self:
+            self.record(name, [])
+
+        if isinstance(self[name], list):
+            self[name].append(value)
+        else:
+            raise KeyError(f"can not collect into {name}, because it is not a list")
+
+    def collect_unique(
+        self, name: Union[str, Tuple[str, ...]], value, ensure_uniqueness: bool = False
+    ):
+        if name not in self:
+            self.record(name, set())
+
+        if isinstance(self[name], set):
+            if not ensure_uniqueness or not (value in self[name]):
+                self[name].add(value)
+        else:
+            raise KeyError(f"can not collect into {name}, because it is not a set")
