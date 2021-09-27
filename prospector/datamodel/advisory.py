9e9339dfd30f9d842dfa0bb7ca2322f112649d7b
Ákos <akos.milankovich@search-lab.hu>
2021-09-27 10:08:49 +0200

    Merge remote-tracking branch 'upstream/spacy-advisory-record'

--- prospector/datamodel/advisory.py
+++ prospector/datamodel/advisory.py
@@ -1,21 +1,54 @@
 # from typing import Tuple
 # from datamodel import BaseModel
+import csv
 import logging
 import re
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Tuple
+from urllib.parse import urlparse
 
 import requests
+import requests_cache
 from pydantic import BaseModel, Field
 
 import log.util
 
-# from commit_processor.constants import RELEVANT_EXTENSIONS
+from .nlp import (
+    extract_path_tokens,
+    extract_products,
+    extract_special_terms,
+    extract_versions,
+)
+
+ALLOWED_SITES = [
+    "github.com",
+    "github.io",
+    "apache.org",
+    "gitlab.org",
+    "cpan.org",
+    "gnome.org",
+    "gnu.org",
+    "nongnu.org",
+    "kernel.org",
+    "rclone.org",
+    "openssl.org",
+    "squid-cache.org",
+    "ossec.net",
+    "readthedocs.io",
+    "atlassian.net",
+    "jira.atlassian.com",
+    "lists.debian.org",
+    "access.redhat.com",
+    "openstack.org",
+    "python.org",
+    "pypi.org",
+    "for.testing.purposes",  # do not remove this, used in mocks for testing purposes
+    "jvndb.jvn.jp",  # for testing: sometimes unreachable
+]
 
 _logger = log.util.init_local_logger()
 
-# TODO use prospector own NVD feed endpoint
 NVD_REST_ENDPOINT = "http://localhost:8000/nvd/vulnerabilities/"
 
 
@@ -30,18 +63,18 @@ class AdvisoryRecord(BaseModel):
     last_modified_timestamp: int = 0
     references: List[str] = Field(default_factory=list)
     references_content: List[str] = Field(default_factory=list)
-    advisory_references: List[str] = Field(default_factory=list)
     affected_products: List[str] = Field(default_factory=list)
     description: Optional[str] = ""
-    preprocessed_vulnerability_description: str = ""
+    preprocessed_description: str = ""
     relevant_tags: List[str] = None
     versions: List[str] = Field(default_factory=list)
     from_nvd: bool = False
     nvd_rest_endpoint: str = NVD_REST_ENDPOINT
     paths: List[str] = Field(default_factory=list)
-    code_tokens: List[str] = Field(default_factory=list)
+    keywords: Tuple[str, ...] = Field(default_factory=tuple)
+    license: str = "UNKNOWN"
 
-    def analyze(self, use_nvd: bool = False):
+    def analyze(self, use_nvd: bool = False, fetch_references=False):
         self.from_nvd = use_nvd
 
         if self.from_nvd:
@@ -50,16 +83,39 @@ class AdvisoryRecord(BaseModel):
         self.versions.extend(
             [v for v in extract_versions(self.description) if v not in self.versions]
         )
+
         self.affected_products = extract_products(self.description)
         self.paths = extract_path_tokens(self.description)
-        self.code_tokens = extract_camelcase_tokens(self.description)
+        self.keywords = extract_special_terms(self.description)
+
+        _logger.debug("References: " + str(self.references))
+        self.references = [
+            r for r in self.references if urlparse(r).hostname in ALLOWED_SITES
+        ]
+        _logger.debug("Relevant references: " + str(self.references))
+
+        if fetch_references:
+            for r in self.references:
+                ref_content = fetch_reference_content(r)
+                if ref_content:
+                    _logger.debug("Fetched content of reference " + r)
+                    self.references_content.append(ref_content)
+
+        self.license = self._guess_license()
+        if self.repository_url == "":
+            self.repository_url = self._guess_repository()
 
     def _get_from_nvd(self, vuln_id: str, nvd_rest_endpoint: str = NVD_REST_ENDPOINT):
         """
         populate object field using NVD data
-        returns: description, published_timestamp, last_modified timestamp, list of links
+        returns: description, published_timestamp, last_modified timestamp, list of references
         """
 
+        # TODO check behavior when some of the data attributes of the AdvisoryRecord
+        # class contain data (e.g. passed explicitly as input by the useer);
+        # In that case, shall the data from NVD be appended to the exiting data,
+        # replace it, be ignored?
+        # (note: right now, it just replaces it)
         try:
             response = requests.get(nvd_rest_endpoint + vuln_id)
             if response.status_code != 200:
@@ -90,85 +146,89 @@ class AdvisoryRecord(BaseModel):
                 exc_info=log.config.level < logging.INFO,
             )
 
+    def _guess_license(self) -> str:
+        lowercase_descr = self.description.lower()
 
-def extract_versions(text) -> "list[str]":
-    """
-    Extract all versions mentioned in the advisory text
-    """
-    regex = r"[0-9]{1,}\.[0-9]{1,}[0-9a-z.]*"
-    result = re.findall(regex, text)
+        oss_keywords = [
+            "apache",
+            "eclipse",
+            "linux",
+            "android",
+            "jenkins",
+            "php",
+            "python",
+            "wireshark",
+            "wordpress",
+            "dom4j",
+            "jackson-data",
+            "jackson-databind",
+            "jetty",
+            "openvpn",
+            "openssl",
+            "debian",
+            "ubuntu",
+            "netty",
+            "spring framework",
+            "spring security",
+        ]
 
-    return result
+        proprietary_keywords = [
+            "microsoft",
+            "windows",
+            "oracle",
+            "ibm",
+            "sap",
+            "salesforce",
+            "cisco",
+            "adobe",
+            "dell",
+            "netgear",
+            "dahua",
+            "intel",
+            "symantec",
+        ]
 
+        matching_oss = [m for m in oss_keywords if (m in lowercase_descr)]
+        matching_proprietary = [
+            m for m in proprietary_keywords if (m in lowercase_descr)
+        ]
 
-def extract_products(text) -> "list[str]":
-    """
-    Extract product names from advisory text
-    """
-    # TODO implement this properly
-    regex = r"([A-Z]+[a-z\b]+)"
-    result = list(set(re.findall(regex, text)))
-    return [p for p in result if len(p) > 2]
+        if matching_oss is not []:
+            return "OSS"
 
+        if matching_proprietary is not []:
+            return "PROPRIETARY"
 
-def extract_camelcase_tokens(text) -> "list[str]":
-    """
-    Extract camelcase or snake_case elements from advisory text
-    """
-    regex = r"([A-Za-z]+[a-z\b]+[A-Z]+[a-z\b]+)"
-    result = list(set(re.findall(regex, text)))
-    # return list(result)
-    return [p for p in result if len(p) > 2]
-    # return["blaHlafaHlsafs"]
+        return "UNKNOWN"
 
+    def _guess_repository(self) -> str:
 
-def extract_path_tokens(text: str, strict_extensions: bool = False) -> List[str]:
-    """
-    Used to look for paths in the text (i.e. vulnerability description)
+        product_repository_map = {}
 
-    Input:
-        text (str)
-        strict_extensions (bool): this function will always extract tokens with (back) slashes,
-            but it will only match single file names if they have the correct extension, if this argument is True
+        with open("./datamodel/gazetteers/product_repositories.csv", mode="r") as f:
+            reader = csv.reader(f)
+            product_repository_map = {rows[0]: rows[1] for rows in reader}
 
-    Returns:
-        list: a list of paths that are found
-    """
-    tokens = re.split(r"\s+", text)  # split the text into words
-    tokens = [
-        token.strip(",.:;-+!?)]}'\"") for token in tokens
-    ]  # removing common punctuation marks
-    tokens = [t for t in tokens if t != ""]
-    paths = []
-    for token in tokens:
-        contains_path_separators = ("\\" in token) or ("/" in token) or ("." in token)
-        separated_with_period = "." in token
-        # has_relevant_extension = token.split(".")[-1] in RELEVANT_EXTENSIONS
-        is_xml_tag = token.startswith("<")
-        is_property = token.endswith("=")
-        is_likely_version = token[0].isdigit() and separated_with_period
+        for p in self.affected_products:
+            if p in product_repository_map:
+                return product_repository_map[p]
 
-        # is_path = contains_path_separators or (
-        #     has_relevant_extension if strict_extensions else separated_with_period
-        # )
-        # probably_not_path = is_xml_tag or is_property or not has_relevant_extension or not contains_path_separators or is_likely_version
-
-        if (
-            is_property
-            or is_xml_tag
-            or is_likely_version
-            or not contains_path_separators
-        ):
-            continue
-
-        # if strict_extensions and not has_relevant_extension:
-        #     continue
-
-        paths.append(token)
-
-    return paths
+        return ""
 
 
+def fetch_reference_content(reference: str) -> str:
+
+    try:
+        session = requests_cache.CachedSession("requests-cache")
+        content = session.get(reference).text
+    except Exception:
+        _logger.debug(f"can not retrieve reference content: {reference}", exc_info=True)
+        return False
+
+    return content
+
+
+# would be used in the future
 @dataclass
 class Reference:
     """
@@ -178,6 +238,9 @@ class Reference:
     url: str
     repo_url: str
 
+    # TODO we do not need a class for this, this is a collection of
+    # functions, with not state at all, they can become part of some
+    # other general string analysis module
     def __post_init__(self):
         # TODO this is not general (the .git suffix can be stripped only for github)
         self.repo_url = re.sub(r"\.git$|/$", "", self.repo_url)
---
+++ prospector/ranking/rule_helpers.py
@@ -0,0 +1,198 @@
+from typing import Set
+
+import pandas
+
+import log.util
+from datamodel.advisory import AdvisoryRecord
+from datamodel.commit import Commit
+from git.git import Git
+from util.similarity import (
+    damerau_levenshtein_edit_distance,
+    jaccard_set_similarity,
+    levenshtein_edit_distance,
+    otsuka_ochiai_set_similarity,
+    sorensen_dice_set_similarity,
+)
+from util.tokenize import tokenize_non_nl_term
+
+_logger = log.util.init_local_logger()
+
+DAYS_BEFORE = 180
+DAYS_AFTER = 365
+DAY_IN_SECONDS = 86400
+
+
+def extract_references_vuln_id(commit: Commit, advisory_record: AdvisoryRecord) -> bool:
+    return advisory_record.vulnerability_id in commit.cve_refs
+
+
+def extract_time_between_commit_and_advisory_record(
+    commit: Commit, advisory_record: AdvisoryRecord
+) -> int:
+    return commit.timestamp - advisory_record.published_timestamp
+
+
+def extract_changed_relevant_paths(
+    commit: Commit, advisory_record: AdvisoryRecord
+) -> Set[str]:
+    """
+    Return the list of the changed paths (by a commit) that are
+    mentioned in the advisory record
+    """
+    relevant_paths = []
+    for advisory_path in advisory_record.paths:
+        relevant_paths += filter(
+            lambda path: advisory_path in path, commit.changed_files
+        )
+
+    return set(relevant_paths)
+
+
+def extract_other_CVE_in_message(
+    commit: Commit, advisory_record: AdvisoryRecord
+) -> Set[str]:
+    return set(commit.cve_refs) - {advisory_record.vulnerability_id}
+
+
+def is_commit_in_given_interval(
+    version_timestamp: int, commit_timestamp: int, day_interval: int
+) -> bool:
+    """
+    Return True if the commit is in the given interval before or after the timestamp
+    """
+
+    if day_interval == 0:
+        return version_timestamp == commit_timestamp
+    elif day_interval > 0:
+        return (
+            version_timestamp + day_interval * DAY_IN_SECONDS
+            >= commit_timestamp
+            >= version_timestamp
+        )
+    else:
+        return (
+            version_timestamp + day_interval * DAY_IN_SECONDS
+            <= commit_timestamp
+            <= version_timestamp
+        )
+
+
+def extract_referred_to_by_nvd(
+    commit: Commit, advisory_record: AdvisoryRecord
+) -> Set[str]:
+    return set(
+        filter(
+            lambda reference: commit.commit_id[:8] in reference,
+            advisory_record.references,
+        )
+    )
+
+
+def is_commit_reachable_from_given_tag(
+    commit: Commit, advisory_record: AdvisoryRecord, version_tag: str
+) -> bool:
+    """
+    Return True if the commit is reachable from the given tag
+    """
+    repo = Git(advisory_record.repository_url)
+    # repo.clone()
+
+    commit_id = commit.commit_id
+    tag_id = repo.get_commit_id_for_tag(version_tag)
+
+    if not repo.get_commits_between_two_commit(
+        commit_id, tag_id
+    ) and not repo.get_commits_between_two_commit(tag_id, commit_id):
+        return False
+
+    return True
+
+
+# def extract_referred_to_by_pages_linked_from_advisories(
+#     commit: Commit, advisory_record: AdvisoryRecord
+# ) -> Set[str]:
+#     allowed_references = filter(
+#         lambda reference: urlparse(reference).hostname in ALLOWED_SITES,
+#         advisory_record.references,
+#     )
+#     session = requests_cache.CachedSession("requests-cache")
+
+#     def is_commit_cited_in(reference: str):
+#         try:
+#             return commit.commit_id[:8] in session.get(reference).text
+#         except Exception:
+#             _logger.debug(f"can not retrieve site: {reference}", exc_info=True)
+#             return False
+#     return set(filter(is_commit_cited_in, allowed_references))
+
+
+def extract_commit_mentioned_in_linked_pages(
+    commit: Commit, advisory_record: AdvisoryRecord
+) -> int:
+
+    # TODO: convert advisory.references to a dictionary (the key must be the url,
+    # else we cannot say in which side we found a reference to the commit at hand,
+    # when we find one);
+    # for now, we can only return an integer from this function, but not ideal
+    matching_references_count = 0
+    for content_of_reference in advisory_record.references_content:
+        if commit.commit_id[:8] in content_of_reference:
+            matching_references_count += 1
+
+    return matching_references_count
+
+
+def extract_path_similarities(commit: Commit, advisory_record: AdvisoryRecord):
+    similarities = pandas.DataFrame(
+        columns=[
+            "changed file",
+            "code token",
+            "jaccard",
+            "sorensen-dice",
+            "otsuka-ochiai",
+            "levenshtein",
+            "damerau-levenshtein",
+            "length diff",
+            "inverted normalized levenshtein",
+            "inverted normalized damerau-levenshtein",
+        ]
+    )
+    for changed_file_path in commit.changed_files:
+        for code_token in advisory_record.keywords:
+            parts_of_file_path = tokenize_non_nl_term(changed_file_path)
+            parts_of_code_token = tokenize_non_nl_term(code_token)
+            similarities = similarities.append(
+                {
+                    "changed file": changed_file_path,
+                    "code token": code_token,
+                    "jaccard": jaccard_set_similarity(
+                        set(parts_of_file_path), set(parts_of_code_token)
+                    ),
+                    "sorensen-dice": sorensen_dice_set_similarity(
+                        set(parts_of_file_path), set(parts_of_code_token)
+                    ),
+                    "otsuka-ochiai": otsuka_ochiai_set_similarity(
+                        set(parts_of_file_path), set(parts_of_code_token)
+                    ),
+                    "levenshtein": levenshtein_edit_distance(
+                        parts_of_file_path, parts_of_code_token
+                    ),
+                    "damerau-levenshtein": damerau_levenshtein_edit_distance(
+                        parts_of_file_path, parts_of_code_token
+                    ),
+                    "length diff": abs(
+                        len(parts_of_file_path) - len(parts_of_code_token)
+                    ),
+                },
+                ignore_index=True,
+            )
+
+    levenshtein_max = similarities["levenshtein"].max()
+    similarities["inverted normalized levenshtein"] = 1 - (
+        similarities["levenshtein"] / levenshtein_max
+    )
+    damerau_levenshtein_max = similarities["damerau-levenshtein"].max()
+    similarities["inverted normalized damerau-levenshtein"] = 1 - (
+        similarities["damerau-levenshtein"] / damerau_levenshtein_max
+    )
+    return similarities
