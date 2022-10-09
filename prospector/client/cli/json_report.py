c811f1f7117c5592f2a6fd53fc8dd8e530dc4eed
Antonino Sabetta (i064196) <antonino.sabetta@sap.com>
2022-10-09 10:02:54 +0200

    fix: bug in production of JSON report

--- prospector/client/cli/json_report.py
+++ prospector/client/cli/json_report.py
@@ -7,13 +7,16 @@ from datamodel.commit import Commit
 _logger = log.util.init_local_logger()
 
 
-def report_as_json(results: "list[Commit]", advisory_record: AdvisoryRecord):
+def report_as_json(
+    results: "list[Commit]",
+    advisory_record: AdvisoryRecord,
+    filename: str = "prospector-report.json",
+):
 
     data = {
         "advisory_record": advisory_record.dict(),
         "commits": [r.dict() for r in results],
     }
-    filename = "prospector-report.json"
     _logger.info("Writing results to " + filename)
     with open(filename, "w", encoding="utf8") as json_file:
         json.dump(data, json_file, ensure_ascii=True, indent=4)
