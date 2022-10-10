10508a95586f7d69a729f588c355a6dc7823280c
sacca97 <tommaso.sacchetti@gmail.com>
2022-10-10 16:56:14 +0200

    fixed json report generation to handle sets and file extensions when default is set

--- prospector/client/cli/main.py
+++ prospector/client/cli/main.py
@@ -8,6 +8,163 @@ path_root = os.getcwd()
 if path_root not in sys.path:
     sys.path.append(path_root)
 
+import log  # noqa: E402
+
+from client.cli.console import ConsoleWriter, MessageStatus  # noqa: E402
+from client.cli.console_report import report_on_console  # noqa: E402
+from client.cli.html_report import report_as_html  # noqa: E402
+from client.cli.json_report import report_as_json  # noqa: E402
+from client.cli.prospector_client import (  # noqa: E402
+    MAX_CANDIDATES,  # noqa: E402
+    TIME_LIMIT_AFTER,  # noqa: E402
+    TIME_LIMIT_BEFORE,  # noqa: E402
+    prospector,  # noqa: E402
+)
+from git.git import GIT_CACHE  # noqa: E402
+from stats.execution import execution_statistics  # noqa: E402
+from util.http import ping_backend  # noqa: E402
+
+_logger = log.util.init_local_logger()
+
+DEFAULT_BACKEND = "http://localhost:8000"
+# VERSION = '0.1.0'
+# SCRIPT_PATH=os.path.dirname(os.path.realpath(__file__))
+# print(SCRIPT_PATH)
+
+
+def parseArguments(args):
+    parser = argparse.ArgumentParser(description="Prospector CLI")
+    parser.add_argument(
+        "vulnerability_id",
+        nargs="?",
+        type=str,
+        help="ID of the vulnerability to analyze",
+    )
+
+    parser.add_argument("--repository", default="", type=str, help="Git repository")
+
+    parser.add_argument(
+        "--pub-date", default="", help="Publication date of the advisory"
+    )
+
+    parser.add_argument("--descr", default="", type=str, help="Text of the advisory")
+
+    parser.add_argument(
+        "--max-candidates",
+        default=MAX_CANDIDATES,
+        type=int,
+        help="Maximum number of candidates to consider",
+    )
+
+    parser.add_argument(
+        "--tag-interval",
+        default="",
+        type=str,
+        help="Tag interval (X,Y) to consider (the commit must be reachable from Y but not from X, and must not be older than X)",
+    )
+
+    parser.add_argument(
+        "--version-interval",
+        default="",
+        type=str,
+        help="Version interval (X,Y) to consider (the corresponding tags will be inferred automatically, and the commit must be reachable from Y but not from X, and must not be older than X)",
+    )
+
+    parser.add_argument(
+        "--modified-files",
+        default="",
+        type=str,
+        help="Files (partial names are ok, comma separated) that the candidate commits are supposed to touch",
+    )
+
+    parser.add_argument(
+        "--filter-extensions",
+        default="java",
+        type=str,
+        help="Filter out commits that do not modify at least one file with this extension",
+    )
+
+    parser.add_argument(
+        "--advisory-keywords",
+        default=None,
+        type=str,
+        help="Add the specified keywords to the advisory record",
+    )
+
+    parser.add_argument(
+        "--use-nvd", default=None, action="store_true", help="Get data from NVD"
+    )
+
+    # FIXME: with python 3.9 we can use the new built-in capabilities of argparse to get rid of this
+    parser.add_argument(
+        "--no-use-nvd", default=None, action="store_true", help="Get data from NVD"
+    )
+
+    parser.add_argument(
+        "--fetch-references",
+        default=True,
+        action="store_true",
+        help="Fetch content of references linked from the advisory",
+    )
+
+    parser.add_argument(
+        "--backend", default=DEFAULT_BACKEND, type=str, help="URL of the backend server"
+    )
+
+    parser.add_argument(
+        "--use-backend",
+        default="always",
+        choices=["always", "never", "optional"],
+        type=str,
+        help="Use the backend server",
+    )
+
+    parser.add_argument(
+        "--report",
+        default="html",
+        choices=["html", "json", "console"],
+        type=str,
+        help="Format of the report (options: console, json, html)",
+    )
+
+    parser.add_argument(
+        "--report-filename",
+        default="prospector-report",
+        type=str,
+        help="File where to save the report",
+    )
+
+    parser.add_argument("-c", "--conf", help="specify configuration file")
+
+    parser.add_argument(
+        "-p", "--ping", help="Contact server to check it's alive", action="store_true"
+    )
+
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        dest="log_level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level",
+    )
+
+    return parser.parse_args(args[1:])
+
+
+def getConfiguration(customConfigFile=None):
+    # simple is better: only one configuration file is
+    # taken into account, no overriding of options from
+    # one file to the other!
+
+    # the order is (as soon as one is found, the rest is ignored):
+    # 1) the file passed as argument to this function
+    # 2) ./prospector.conf
+    # 3) ~/.prospector/conf
+
+    localConfigFile = os.path.join(os.getcwd(), "prospector.conf")
+    userConfigFile = os.path.join(Path.home(), ".prospector/conf")
+
+    config = configparser.ConfigParser()
 
 # Loading .env file before doint anything else
 # load_dotenv()
@@ -97,8 +254,25 @@ def main(argv):  # noqa: C901
         rules=["ALL"],
     )
 
-    if config.preprocess_only:
-        return
+    with ConsoleWriter("Generating report") as console:
+        report_file = None
+        if report == "console":
+            report_on_console(results, advisory_record, log.config.level < logging.INFO)
+        elif report == "json":
+            report_file = report_as_json(
+                results, advisory_record, args.report_filename + ".json"
+            )
+        elif report == "html":
+            report_file = report_as_html(
+                results, advisory_record, args.report_filename + ".html"
+            )
+        else:
+            _logger.warning("Invalid report type specified, using 'console'")
+            console.set_status(MessageStatus.WARNING)
+            console.print(
+                f"{report} is not a valid report type, 'console' will be used instead",
+            )
+            report_on_console(results, advisory_record, log.config.level < logging.INFO)
 
     with ConsoleWriter("Generating report") as console:
         match config.report:
