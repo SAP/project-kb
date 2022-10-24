0c49bdb158d40ebd37db0536a9c46f88e67b185c
sacca97 <tommaso.sacchetti@gmail.com>
2022-10-24 16:59:14 +0200

    deleted some useless stuff, renamed test files

--- prospector/util/collection.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Tuple, Union
-
-
-def union_of(base: Union[List, Tuple], newer: Union[List, Tuple]) -> Union[List, Tuple]:
-    if isinstance(base, list):
-        return list(set(base) | set(newer))
-    elif isinstance(base, tuple):
-        return tuple(set(base) | set(newer))
