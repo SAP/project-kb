15f88d4426aefff7b214a0ee8ad7d1b2bd564bec
sacca97 <tommaso.sacchetti@gmail.com>
2022-10-25 15:53:32 +0200

    back to basemodel usage

--- prospector/api/routers/preprocessed.py
+++ prospector/api/routers/preprocessed.py
@@ -23,7 +23,7 @@ async def get_commits(
     data = db.lookup(repository_url, commit_id)
 
     if len(data) == 0:
-        raise HTTPException(status_code=404, detail="Commit not found")
+        return JSONResponse(status_code=404, content={"message": "Not found"})
 
     return JSONResponse(data)
 
