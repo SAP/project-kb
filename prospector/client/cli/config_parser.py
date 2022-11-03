6621e37e702ca66542812d081d24c9262263481a
sacca97 <tommaso.sacchetti@gmail.com>
2022-11-03 22:22:31 +0100

    omegaconf first implementation

---
+++ prospector/client/cli/config_parser.py
@@ -0,0 +1,208 @@
+import argparse
+from dataclasses import dataclass
+import os
+from typing import Dict
+
+from omegaconf import OmegaConf
+from log.logger import logger
+from client.cli.prospector_client import DEFAULT_BACKEND, MAX_CANDIDATES
+
+
+def parse_cli_args(args) -> Dict[str, any]:
+    parser = argparse.ArgumentParser(description="Prospector CLI")
+    parser.add_argument(
+        "cve_id",
+        nargs="?",
+        type=str,
+        help="ID of the vulnerability to analyze",
+    )
+
+    parser.add_argument("--repository", default="", type=str, help="Git repository url")
+
+    parser.add_argument(
+        "--preprocess-only",
+        action="store_true",
+        help="Commit preprocessing only",
+    )
+
+    parser.add_argument("--pub-date", type=str, help="Publication date of the advisory")
+
+    parser.add_argument("--description", type=str, help="Advisory description")
+
+    parser.add_argument(
+        "--max-candidates",
+        type=int,
+        help="Maximum number of candidates to consider",
+    )
+
+    parser.add_argument(
+        "--tag-interval",
+        type=str,
+        help="Tag interval (X,Y) to consider (the commit must be reachable from Y but not from X, and must not be older than X)",
+    )
+
+    parser.add_argument(
+        "--version-interval",
+        type=str,
+        help="Version interval (X,Y) to consider (the corresponding tags will be inferred automatically, and the commit must be reachable from Y but not from X, and must not be older than X)",
+    )
+
+    parser.add_argument(
+        "--modified-files",
+        type=str,
+        help="Names (or partial names) comma separated that the commits are supposed to modify",
+    )
+
+    parser.add_argument(
+        "--filter-extensions",
+        type=str,
+        help="Filter out commits that do not modify at least one file with this extension",
+    )
+
+    parser.add_argument(
+        "--keywords",
+        type=str,
+        help="Consider these specific keywords",
+    )
+
+    parser.add_argument(
+        "--use-nvd",
+        action=argparse.BooleanOptionalAction,
+        help="Get data from NVD",
+    )
+
+    parser.add_argument(
+        "--fetch-references",
+        action="store_true",
+        help="Fetch content of references linked from the advisory",
+    )
+
+    parser.add_argument("--backend", type=str, help="URL of the backend server")
+
+    parser.add_argument(
+        "--use-backend",
+        default="always",
+        choices=["always", "never", "optional"],
+        type=str,
+        help="Use the backend server",
+    )
+
+    parser.add_argument(
+        "--report",
+        choices=["html", "json", "console", "allfiles"],
+        type=str,
+        help="Format of the report (options: console, json, html)",
+    )
+
+    parser.add_argument(
+        "--report-filename",
+        type=str,
+        help="File where to save the report",
+    )
+
+    parser.add_argument(
+        "-c", "--config", default="config.yaml", help="Configuration file"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--ping",
+        help="Ping the server to check if it's online",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Set the logging level",
+    )
+
+    return vars(parser.parse_args())
+
+
+def parse_config_file(filename: str):
+    if os.path.isfile(filename):
+        logger.info(f"Loading configuration from {filename}")
+        config = OmegaConf.load(filename)
+        return config
+
+    return None
+
+
+@dataclass
+class Config:
+    def __init__(
+        self,
+        cve_id: str,
+        repository: str,
+        preprocess_only: bool,
+        pub_date: str,
+        description: str,
+        max_candidates: int,
+        tag_interval: str,
+        version_interval: str,
+        modified_files: str,
+        filter_extensions: str,
+        keywords: str,
+        use_nvd: bool,
+        fetch_references: bool,
+        backend: str,
+        use_backend: str,
+        report: str,
+        report_filename: str,
+        ping: bool,
+        log_level: str,
+        git_cache: str,
+    ):
+        self.cve_id = cve_id
+        self.repository = repository
+        self.preprocess_only = preprocess_only
+        self.pub_date = pub_date
+        self.description = description
+        self.max_candidates = max_candidates
+        self.tag_interval = tag_interval
+        self.version_interval = version_interval
+        self.modified_files = modified_files
+        self.filter_extensions = filter_extensions
+        self.keywords = keywords
+        self.use_nvd = use_nvd
+        self.fetch_references = fetch_references
+        self.backend = backend
+        self.use_backend = use_backend
+        self.report = report
+        self.report_filename = report_filename
+        self.ping = ping
+        self.log_level = log_level
+        self.git_cache = git_cache
+
+
+def get_configuration(argv):
+    args = parse_cli_args(argv)
+    conf = parse_config_file(args.get("config"))
+    if conf is None:
+        return False
+    return Config(
+        cve_id=args.get("cve_id") or conf.get("cve_id"),
+        repository=args.get("repository") or conf.get("repository"),
+        preprocess_only=args.get("preprocess_only") or conf.get("preprocess_only"),
+        pub_date=args.get("pub_date") or conf["advisory"].get("pub_date"),
+        description=args.get("description") or conf["advisory"].get("description"),
+        max_candidates=args.get("max_candidates") or conf.get("max_candidates"),
+        tag_interval=args.get("tag_interval") or conf.get("tag_interval"),
+        version_interval=args.get("version_interval") or conf.get("version_interval"),
+        modified_files=args.get("modified_files")
+        or conf["advisory"].get("modified_files"),
+        filter_extensions=args.get("filter_extensions")
+        or conf.get("filter_extensions"),
+        keywords=args.get("keywords") or conf["advisory"].get("keywords"),
+        use_nvd=args.get("use_nvd") or conf.get("use_nvd"),
+        fetch_references=args.get("fetch_references") or conf.get("fetch_references"),
+        backend=args.get("backend") or conf.get("backend"),
+        use_backend=args.get("use_backend") or conf.get("use_backend"),
+        report=args.get("report") or conf["report"].get("format"),
+        report_filename=args.get("report_filename") or conf["report"].get("name"),
+        ping=args.get("ping") or conf.get("ping"),
+        git_cache=args.get("git_cache") or conf.get("git_cache"),
+        log_level=args.get("log_level") or conf.get("log_level"),
+    )
--- prospector/client/cli/main.py
+++ prospector/client/cli/main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import argparse
-import configparser
 import logging
 import os
 import signal
@@ -8,6 +6,7 @@ import sys
 from pathlib import Path
 from typing import Any, Dict
 from dotenv import load_dotenv
+from omegaconf import OmegaConf
 
 
 path_root = os.getcwd()
@@ -19,11 +18,11 @@ load_dotenv()
 
 # Load logger before doing anything else
 from log.logger import logger, get_level, pretty_log  # noqa: E402
+from client.cli.config_parser import get_configuration, parse_cli_args  # noqa: E402
 
 from client.cli.console import ConsoleWriter, MessageStatus  # noqa: E402
 from client.cli.report import as_json, as_html, report_on_console  # noqa: E402
 from client.cli.prospector_client import (  # noqa: E402
-    MAX_CANDIDATES,  # noqa: E402
     TIME_LIMIT_AFTER,  # noqa: E402
     TIME_LIMIT_BEFORE,  # noqa: E402
     DEFAULT_BACKEND,  # noqa: E402
@@ -34,318 +33,98 @@ from stats.execution import execution_statistics  # noqa: E402
 from util.http import ping_backend  # noqa: E402
 
 
-# VERSION = '0.1.0'
-# SCRIPT_PATH=os.path.dirname(os.path.realpath(__file__))
-# print(SCRIPT_PATH)
-
-
-def parseArguments(args):
-    parser = argparse.ArgumentParser(description="Prospector CLI")
-    parser.add_argument(
-        "vulnerability_id",
-        nargs="?",
-        type=str,
-        help="ID of the vulnerability to analyze",
-    )
-
-    parser.add_argument("--repository", default="", type=str, help="Git repository")
-
-    parser.add_argument(
-        "--find-twin",
-        default="",
-        type=str,
-        help="Lookup for a twin of the specified commit",
-    )
-
-    parser.add_argument(
-        "--preprocess-only",
-        action="store_true",
-        help="Only preprocess the commits for the specified repository",
-    )
-
-    parser.add_argument(
-        "--pub-date", default="", help="Publication date of the advisory"
-    )
-
-    parser.add_argument("--descr", default="", type=str, help="Text of the advisory")
-
-    parser.add_argument(
-        "--max-candidates",
-        default=MAX_CANDIDATES,
-        type=int,
-        help="Maximum number of candidates to consider",
-    )
-
-    parser.add_argument(
-        "--tag-interval",
-        default="",
-        type=str,
-        help="Tag interval (X,Y) to consider (the commit must be reachable from Y but not from X, and must not be older than X)",
-    )
-
-    parser.add_argument(
-        "--version-interval",
-        default="",
-        type=str,
-        help="Version interval (X,Y) to consider (the corresponding tags will be inferred automatically, and the commit must be reachable from Y but not from X, and must not be older than X)",
-    )
-
-    parser.add_argument(
-        "--modified-files",
-        default="",
-        type=str,
-        help="Files (partial names are ok, comma separated) that the candidate commits are supposed to touch",
-    )
-
-    parser.add_argument(
-        "--filter-extensions",
-        default="",
-        type=str,
-        help="Filter out commits that do not modify at least one file with this extension",
-    )
-
-    parser.add_argument(
-        "--advisory-keywords",
-        default=None,
-        type=str,
-        help="Add the specified keywords to the advisory record",
-    )
-
-    parser.add_argument(
-        "--use-nvd", default=None, action="store_true", help="Get data from NVD"
-    )
-
-    # FIXME: with python 3.9 we can use the new built-in capabilities of argparse to get rid of this
-    parser.add_argument(
-        "--no-use-nvd", default=None, action="store_true", help="Get data from NVD"
-    )
-
-    parser.add_argument(
-        "--fetch-references",
-        default=True,
-        action="store_true",
-        help="Fetch content of references linked from the advisory",
-    )
-
-    parser.add_argument(
-        "--backend", default=DEFAULT_BACKEND, type=str, help="URL of the backend server"
-    )
-
-    parser.add_argument(
-        "--use-backend",
-        default="always",
-        choices=["always", "never", "optional"],
-        type=str,
-        help="Use the backend server",
-    )
-
-    parser.add_argument(
-        "--report",
-        default="html",
-        choices=["html", "json", "console", "allfiles"],
-        type=str,
-        help="Format of the report (options: console, json, html)",
-    )
-
-    parser.add_argument(
-        "--report-filename",
-        default="prospector-report",
-        type=str,
-        help="File where to save the report",
-    )
-
-    parser.add_argument("-c", "--conf", help="specify configuration file")
-
-    parser.add_argument(
-        "-p", "--ping", help="Contact server to check it's alive", action="store_true"
-    )
-
-    parser.add_argument(
-        "-l",
-        "--log-level",
-        dest="log_level",
-        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        help="Set the logging level",
-    )
-
-    return parser.parse_args()
-
-
-def getConfiguration(customConfigFile=None):
-    # simple is better: only one configuration file is
-    # taken into account, no overriding of options from
-    # one file to the other!
-
-    # the order is (as soon as one is found, the rest is ignored):
-    # 1) the file passed as argument to this function
-    # 2) ./prospector.conf
-    # 3) ~/.prospector/conf
-
-    localConfigFile = os.path.join(os.getcwd(), "prospector.conf")
-    userConfigFile = os.path.join(Path.home(), ".prospector/conf")
-
-    config = configparser.ConfigParser()
-
-    if customConfigFile and os.path.isfile(customConfigFile):
-        configFile = customConfigFile
-    elif os.path.isfile(localConfigFile):
-        configFile = localConfigFile
-    elif os.path.isfile(userConfigFile):
-        configFile = userConfigFile
-    else:
-        return None
-
-    logger.info("Loading configuration from " + configFile)
-    config.read(configFile)
-    return parse_config(config)
-
-
-def parse_config(configuration: configparser.ConfigParser) -> Dict[str, Any]:
-    """Parse the configuration file and return the options as a dictionary."""
-    options = {}
-    for section in configuration.sections():
-        for option in configuration.options(section):
-            try:
-                options[option] = configuration.getboolean(section, option)
-            except ValueError:
-                options[option] = configuration.get(section, option)
-    return options
-
-
 def main(argv):  # noqa: C901
     with ConsoleWriter("Initialization") as console:
-        args = parseArguments(argv)
+        config = get_configuration(argv)
 
-        if args.log_level:
-            logger.setLevel(args.log_level)
+        if not config:
+            logger.error("No configuration file found. Cannot proceed.")
 
-        logger.info(f"global log level is set to {get_level(string=True)}")
+            console.print(
+                "No configuration file found.",
+                status=MessageStatus.ERROR,
+            )
+            return
 
-        if args.vulnerability_id is None:
+        logger.setLevel(config.log_level)
+        logger.info(f"Global log level set to {get_level(string=True)}")
+
+        if config.cve_id is None:
             logger.error("No vulnerability id was specified. Cannot proceed.")
             console.print(
                 "No vulnerability id was specified. Cannot proceed.",
                 status=MessageStatus.ERROR,
             )
-            return False
+            return
 
-        configuration = getConfiguration(args.conf)
+        # if args.get("ping"):
+        #     return ping_backend(backend, get_level() < logging.INFO)
 
-        if configuration is None:
-            logger.error("Invalid configuration, exiting.")
-            return False
-
-        report = args.report or configuration.get("report")
-        report_filename = args.report_filename or configuration.get("report_filename")
-
-        nvd_rest_endpoint = configuration.get("nvd_rest_endpoint", "")  # default ???
-
-        backend = args.backend or configuration.get("backend", DEFAULT_BACKEND)  # ???
-
-        use_backend = args.use_backend
-
-        if args.ping:
-            return ping_backend(backend, get_level() < logging.INFO)
-
-        vulnerability_id = args.vulnerability_id
-        repository_url = args.repository
-        vuln_descr = args.descr
-        filter_extensions = args.filter_extensions.split(",")
-
-        # if no backend the filters on the advisory do not work
-        use_nvd = False
-        if args.vulnerability_id.casefold().startswith("cve-"):
-            use_nvd = True
-        if args.use_nvd is True:
-            use_nvd = True
-        elif args.no_use_nvd is True:
-            use_nvd = False
-
-        fetch_references = args.fetch_references or configuration.get(
-            "fetch_references", False
+        config.pub_date = (
+            config.pub_date + "T00:00:00Z" if config.pub_date != "" else ""
         )
 
-        tag_interval = args.tag_interval
-        version_interval = args.version_interval
         time_limit_before = TIME_LIMIT_BEFORE
         time_limit_after = TIME_LIMIT_AFTER
-        max_candidates = args.max_candidates
-        modified_files = args.modified_files.split(",") if args.modified_files else []
-        advisory_keywords = (
-            args.advisory_keywords.split(",") if args.advisory_keywords else []
-        )
 
-        publication_date = ""
-        if args.pub_date != "":
-            publication_date = args.pub_date + "T00:00Z"
-        # if the date is forced manually, the time interval can
-        # be restricted
-        # time_limit_before = int(time_limit_before / 5)
-        # time_limit_after = int(time_limit_after / 2)
-
-        git_cache = os.getenv("GIT_CACHE", default=GIT_CACHE)
-
-        git_cache = configuration.get("git_cache", git_cache)
+        git_cache = os.getenv("GIT_CACHE") or config.git_cache
 
         logger.debug("Using the following configuration:")
-        pretty_log(logger, configuration)
+        pretty_log(logger, config.__dict__)
 
-        logger.debug("Vulnerability ID: " + vulnerability_id)
+        logger.debug("Vulnerability ID: " + config.cve_id)
         logger.debug("time-limit before: " + str(time_limit_before))
         logger.debug("time-limit after: " + str(time_limit_after))
 
-        active_rules = ["ALL"]
-
     results, advisory_record = prospector(
-        vulnerability_id=vulnerability_id,
-        repository_url=repository_url,
-        publication_date=publication_date,
-        vuln_descr=vuln_descr,
-        tag_interval=tag_interval,
-        filter_extensions=filter_extensions,
-        version_interval=version_interval,
-        modified_files=set(modified_files),
-        advisory_keywords=set(advisory_keywords),
+        vulnerability_id=config.cve_id,
+        repository_url=config.repository,
+        publication_date=config.pub_date,
+        vuln_descr=config.description,
+        tag_interval=config.tag_interval,
+        filter_extensions=config.filter_extensions.split(","),
+        version_interval=config.version_interval,
+        modified_files=set(config.modified_files.split(",")),
+        advisory_keywords=set(config.keywords.split(",")),
         time_limit_before=time_limit_before,
         time_limit_after=time_limit_after,
-        use_nvd=use_nvd,
-        nvd_rest_endpoint=nvd_rest_endpoint,
-        fetch_references=fetch_references,
-        backend_address=backend,
-        use_backend=use_backend,
+        use_nvd=config.use_nvd,
+        nvd_rest_endpoint="",
+        fetch_references=config.fetch_references,
+        backend_address=config.backend,
+        use_backend=config.use_backend,
         git_cache=git_cache,
-        limit_candidates=max_candidates,
-        rules=active_rules,
+        limit_candidates=config.max_candidates,
+        rules=["ALL"],
     )
 
-    if args.preprocess_only:
-        return True
+    if config.preprocess_only:
+        return
 
     with ConsoleWriter("Generating report") as console:
-        report_file = None
-        if report == "console":
-            report_on_console(results, advisory_record, get_level() < logging.INFO)
-        elif report == "json":
-            report_file = as_json(results, advisory_record, report_filename)
-        elif report == "html":
-            report_file = as_html(results, advisory_record, report_filename)
-        elif report == "allfiles":
-            as_json(results, advisory_record, report_filename)
-            as_html(results, advisory_record, report_filename)
-        else:
-            logger.warning("Invalid report type specified, using 'console'")
-            console.set_status(MessageStatus.WARNING)
-            console.print(
-                f"{report} is not a valid report type, 'console' will be used instead",
-            )
-            report_on_console(results, advisory_record, get_level() < logging.INFO)
+        match config.report:
+            case "console":
+                report_on_console(results, advisory_record, get_level() < logging.INFO)
+            case "json":
+                as_json(results, advisory_record, config.report_filename)
+            case "html":
+                as_html(results, advisory_record, config.report_filename)
+            case "allfiles":
+                as_json(results, advisory_record, config.report_filename)
+                as_html(results, advisory_record, config.report_filename)
+            case _:
+                logger.warning("Invalid report type specified, using 'console'")
+                console.set_status(MessageStatus.WARNING)
+                console.print(
+                    f"{config.report} is not a valid report type, 'console' will be used instead",
+                )
+                report_on_console(results, advisory_record, get_level() < logging.INFO)
 
         logger.info("\n" + execution_statistics.generate_console_tree())
         execution_time = execution_statistics["core"]["execution time"][0]
         console.print(f"Execution time: {execution_time:.4f} sec")
-        if report_file:
-            console.print(f"Report saved in {report_file}")
-        return True
+        console.print(f"Report saved in {config.report_filename}.{config.report}")
+        return
 
 
 def signal_handler(signal, frame):
