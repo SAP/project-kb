b65934f425f54f45e7b0a81d28d7d44d234f4416
sacca97 <tommaso.sacchetti@gmail.com>
2022-11-07 11:29:09 +0100

    advisory versions extracted from the dedicated section (if available), now matching subdomains of allowed websites, removed unused fields for now, only uses new NVD api

--- prospector/datamodel/advisory_test.py
+++ prospector/datamodel/advisory_test.py
@@ -1,36 +1,4 @@
-# from dataclasses import asdict
-
-import pytest
-from datamodel.advisory import (
-    LOCAL_NVD_REST_ENDPOINT,
-    AdvisoryRecord,
-    build_advisory_record,
-)
-
-# import pytest
-
-
-def test_advisory_basic():
-    adv_rec = AdvisoryRecord(
-        vulnerability_id="CVE-2015-5612",
-        repository_url="https://github.com/abc/xyz",
-        references=[
-            "https://github.com/abc/def/commit/af542817cb325173410876aa3",
-            "https://github.com/abc/def/issues/54",
-        ],
-    )
-
-    assert adv_rec.repository_url == "https://github.com/abc/xyz"
-
-    mentions_commit = False
-    for r in adv_rec.references:
-        if "af542817c" in r:
-            mentions_commit = True
-
-    assert mentions_commit
-    # assert ar.vulnerability_id == "CVE-2015-5612"
-    # assert ar.published_timestamp == "2015-09-04T15:59Z"
-
+from datamodel.advisory import AdvisoryRecord, build_advisory_record
 
 ADVISORY_TEXT = """Unspecified vulnerability in Uconnect before 15.26.1, as used
     in certain Fiat Chrysler Automobiles (FCA) from 2013 to 2015 models, allows
@@ -48,65 +16,28 @@ but not further above (thus "limited" path traversal), if the calling code would
 use the result to construct a path value."""
 
 
-def test_adv_record_versions():
-
-    record = AdvisoryRecord(vulnerability_id="CVE-2014-0050", description=ADVISORY_TEXT)
-    record.analyze()
-
-    assert "15.26.1" in record.versions
-    assert "15.26" not in record.versions
-
-
-def test_adv_record_keywords():
-    record = AdvisoryRecord(
-        vulnerability_id="CVE-XXXX-YYYY", description=ADVISORY_TEXT_2
+def test_advisory_basic():
+    adv = AdvisoryRecord(
+        cve_id="CVE-2015-5612",
+        references=[
+            "https://github.com/abc/def/commit/af542817cb325173410876aa3",
+            "https://github.com/abc/def/issues/54",
+        ],
     )
-    record.analyze()
-
-    # TODO replace when NLP implementation is done
-    # see, https://github.com/SAP/project-kb/issues/256#issuecomment-927639866
-    # assert record.keywords == () or sorted(record.keywords) == sorted(
-    #     (
-    #         "IO",
-    #         "2.7,",
-    #         "FileNameUtils.normalize",
-    #         '"//../foo",',
-    #         '"\\..\\foo",',
-    #         '"limited"',
-    #     )
-    # )
+    assert adv.cve_id == "CVE-2015-5612"
+    assert "https://github.com/abc/def/issues/54" in adv.references
 
 
-def test_build():
-    record = build_advisory_record(
-        "CVE-2022-2839", "", "", LOCAL_NVD_REST_ENDPOINT, True, True, "", "", "", ""
-    )
-    assert record.vulnerability_id == "CVE-2022-2839"
+def test_get_advisory():
+    advisory = AdvisoryRecord("CVE-2021-22696")
+    advisory.get_advisory()
+    print(advisory.__dict__)
+    assert advisory.cve_id == "CVE-2021-22696"
+    assert ("3.4.0", "3.4.3") in advisory.versions
 
 
-@pytest.mark.skip(
-    reason="Easily fails due to NVD API rate limiting or connection issues"
-)
-def test_filenames_extraction():
-    result1 = build_advisory_record(
-        "CVE-2014-0050", "", "", LOCAL_NVD_REST_ENDPOINT, "", True, "", "", "", ""
-    )
-    result2 = build_advisory_record(
-        "CVE-2021-22696", "", "", LOCAL_NVD_REST_ENDPOINT, "", True, "", "", "", ""
-    )
-    result3 = build_advisory_record(
-        "CVE-2021-27582", "", "", LOCAL_NVD_REST_ENDPOINT, "", True, "", "", "", ""
-    )
-    result4 = build_advisory_record(
-        "CVE-2021-29425", "", "", LOCAL_NVD_REST_ENDPOINT, "", True, "", "", "", ""
-    )
-    result5 = build_advisory_record(
-        "CVE-2021-30468", "", "", LOCAL_NVD_REST_ENDPOINT, "", True, "", "", "", ""
-    )
-    assert result1.files == set(["MultipartStream", "FileUpload"])  # Content-Type
-    assert result2.files == set(["JwtRequestCodeFilter", "request_uri"])
-    assert result3.files == set(
-        ["OAuthConfirmationController", "@ModelAttribute", "authorizationRequest"]
-    )
-    assert result4.files == set(["FileNameUtils"])
-    assert result5.files == set(["JsonMapObjectReaderWriter"])
+def test_build_advisory_record():
+    advisory = build_advisory_record("CVE-2014-0050")
+    print(advisory.__dict__)
+    assert advisory.cve_id == "CVE-2014-0050"
+    assert advisory.versions == []
