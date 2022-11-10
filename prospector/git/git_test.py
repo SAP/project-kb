10373f3d576e03404f0a57803ccf884d3b8dbe7c
sacca97 <tommaso.sacchetti@gmail.com>
2022-11-10 09:44:13 +0100

    now computing hash on the first 50 chars of the commit msg, added filtering for /docs/ and .md files in create commits

--- prospector/git/git_test.py
+++ prospector/git/git_test.py
@@ -11,6 +11,10 @@ from .version_to_tag import get_tag_for_version
 
 NEBULA = "https://github.com/slackhq/nebula"
 BEAM = "https://github.com/apache/beam"
+OPENCAST = "https://github.com/opencast/opencast"
+OPENCAST_COMMIT = "bbb473f34ab95497d6c432c81285efb0c739f317"
+
+
 COMMIT_ID = "4645e6034b9c88311856ee91d19b7328bd5878c1"
 COMMIT_ID_1 = "d85e24f49f9efdeed5549a7d0874e68155e25301"
 COMMIT_ID_2 = "b38bd36766994715ac5226bfa361cd2f8f29e31e"
@@ -19,7 +23,7 @@ COMMIT_ID_3 = "ae3ee42469b7c48848d841386ca9c74b7d6bbcd8"
 
 @pytest.fixture
 def repository() -> Git:
-    repo = Git(NEBULA)  # apache/beam
+    repo = Git(OPENCAST)  # apache/beam
     repo.clone()
     return repo
 
@@ -41,6 +45,14 @@ def test_show_tags(repository: Git):
 
     except Exception:
         print("simola")
+
+
+def test_get_tags_for_commit(repository: Git):
+    commits = repository.create_commits()
+    commit = commits.get(OPENCAST_COMMIT)
+    if commit is not None:
+        tags = commit.find_tag("8.1")
+        raise Exception(tags)
 
 
 def test_create_commits(repository: Git):
