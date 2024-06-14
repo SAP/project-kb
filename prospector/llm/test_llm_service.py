8b351252692cf7be62f02fa5b95789b0fd8ec6fb
I748376 <laura.schauer@sap.com>
2024-06-14 08:39:57 +0000

    adds tests

--- prospector/llm/test_llm_service.py
+++ prospector/llm/test_llm_service.py
@@ -1,5 +1,11 @@
+import pytest
+from langchain_core.language_models.llms import LLM
+
 from llm.llm_service import LLMService  # this is a singleton
+from llm.models.gemini import Gemini
+from llm.models.mistral import Mistral
 from llm.models.openai import OpenAI
+from util.singleton import Singleton
 
 
 # Mock the llm_service configuration object
@@ -14,19 +20,66 @@ class Config:
         self.temperature = temperature
 
 
-# Vulnerability ID
-vuln_id = "CVE-2024-32480"
+test_vuln_id = "CVE-2024-32480"
+
+
+@pytest.fixture(autouse=True)
+def reset_singletons():
+    # Clean up singleton instances after each test
+    Singleton._instances = {}
 
 
 class TestModel:
-    def test_sap_gpt4_instantiation(self):
-        config = Config("sap", "gpt-4", "0.0")
+    def test_sap_gpt_instantiation(self):
+        config = Config("sap", "gpt-4", 0.0)
         llm_service = LLMService(config)
         assert isinstance(llm_service._model, OpenAI)
 
-    # def test_invoke_fail(self):
-    #     with pytest.raises(SystemExit):
-    #         config = Config("sap", "gpt-35-turbo", "0.0")
-    #         model = create_model_instance(config)
-    #         vuln_id = "random"
-    #         get_repository_url(model=model, vuln_id=vuln_id)
+    def test_sap_gemini_instantiation(self):
+        config = Config("sap", "gemini-1.0-pro", 0.0)
+        llm_service = LLMService(config)
+        assert isinstance(llm_service._model, Gemini)
+
+    def test_sap_mistral_instantiation(self):
+        config = Config("sap", "mistralai--mixtral-8x7b-instruct-v01", 0.0)
+        llm_service = LLMService(config)
+        assert isinstance(llm_service._model, Mistral)
+
+    def test_singleton_instance_creation(self):
+        """A second instantiation should return the exisiting instance."""
+        config = Config("sap", "gpt-4", 0.0)
+        llm_service = LLMService(config)
+        same_service = LLMService(config)
+        assert (
+            llm_service is same_service
+        ), "LLMService should return the same instance."
+
+    def test_singleton_same_instance(self):
+        """A second instantiation with different parameters should return the existing instance unchanged."""
+        config = Config("sap", "gpt-4", 0.0)
+        llm_service = LLMService(config)
+        config = Config(
+            "sap", "gpt-35-turbo", 0.0
+        )  # This instantiation should not work, but instead return the already existing instance
+        same_service = LLMService(config)
+        assert llm_service is same_service
+        assert llm_service._model.model_name == "gpt-4"
+
+    def test_singleton_retains_state(self):
+        """Reassigning a field variable of the instance should be allowed and reflected
+        across instantiations."""
+        config = Config("sap", "gpt-4", 0.0)
+        service = LLMService(config)
+
+        service._model = OpenAI(
+            model_name="gpt-35-turbo",
+            deployment_url="deployment_url_placeholder",
+            temperature=0.7,
+        )
+        same_service = LLMService(config)
+
+        assert same_service._model == OpenAI(
+            model_name="gpt-35-turbo",
+            deployment_url="deployment_url_placeholder",
+            temperature=0.7,
+        ), "LLMService should retain state between instantiations"
