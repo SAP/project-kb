a546b08a41f4b364d5b06f6c6c36abd618d35fab
I748376 <laura.schauer@sap.com>
2024-06-17 14:29:54 +0000

    sends requests to commit classification service for the top 3 commits

--- prospector/rules/llm/llm_phase.py
+++ prospector/rules/llm/llm_phase.py
@@ -3,9 +3,11 @@ from typing import List
 from datamodel.advisory import AdvisoryRecord
 from datamodel.commit import Commit, apply_ranking
 from llm.llm_service import LLMService
+from llm.model_instantiation import create_model_instance
 from rules.llm.llm_rules import CommitIsSecurityRelevant, LLMRule
 from rules.phase import Phase, rule_statistics
 from stats.execution import Counter
+from util.config_parser import LLMServiceConfig
 
 LLM_RULES: List[LLMRule] = [CommitIsSecurityRelevant("COMMIT_IS_SECURITY_RELEVANT", 32)]
 
@@ -13,10 +15,11 @@ LLM_RULES: List[LLMRule] = [CommitIsSecurityRelevant("COMMIT_IS_SECURITY_RELEVAN
 class LLMPhase(Phase):
     """Implementes the NLP phase in which all NLP rules are applied."""
 
-    def __init__(self, llm_service: LLMService):
+    def __init__(self, llm_service_config: LLMServiceConfig):
         super().__init__("LLM Phase")
         self.rules = LLM_RULES
-        self._llm_service = llm_service
+        self._llm_service_config = llm_service_config
+        self._llm_service = create_model_instance(llm_service_config)
 
     def apply_rules(self, candidates: List[Commit], rules=["ALL"]) -> List[Commit]:
         enabled_rules = (
@@ -29,7 +32,9 @@ class LLMPhase(Phase):
             counter.initialize("matches", unit="matches")
             for candidate in candidates:
                 for rule in enabled_rules:
-                    if rule.apply(candidate, self._llm_service):
+                    if rule.apply(
+                        candidate, self._llm_service, self._llm_service_config
+                    ):
                         counter.increment("matches")
                         candidate.add_match(rule.as_dict())
                 candidate.compute_relevance()
