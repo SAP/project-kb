beab86e329ad75b92a6050a7108c05973269d830
I748376 <laura.schauer@sap.com>
2024-06-19 09:47:27 +0000

    corrects mistake in singleton logger value

--- prospector/util/singleton.py
+++ prospector/util/singleton.py
@@ -9,8 +9,8 @@ class Singleton(type):
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        if cls not in cls._instances:
-            logger.error(
+        else:
+            logger.info(
                 f"Cannot instantiate a Singleton twice. Returning already existing instance of class {cls}."
             )
         return cls._instances[cls]
