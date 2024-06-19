dc0ada7ac6fb1c6e429c65324fb224cb8bffea7a
I748376 <laura.schauer@sap.com>
2024-06-19 14:30:32 +0000

    adds correct strings to return from _llm_type()

--- prospector/llm/models/openai.py
+++ prospector/llm/models/openai.py
@@ -15,7 +15,7 @@ class OpenAI(LLM):
 
     @property
     def _llm_type(self) -> str:
-        return "custom"
+        return "SAP OpenAI"
 
     @property
     def _identifying_params(self) -> Dict[str, Any]:
