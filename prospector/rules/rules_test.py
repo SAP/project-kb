1ab8267fc4792be7b0e4e7a8fd9faecf1421a5f6
I748376 <laura.schauer@sap.com>
2024-06-21 14:46:26 +0000

    updates tests for rules - not all rules are covered

---
+++ prospector/rules/rules_test.py
@@ -0,0 +1,136 @@
+from typing import List
+
+import pytest
+from requests_cache import Optional
+
+from datamodel.advisory import AdvisoryRecord
+from datamodel.commit import Commit
+from rules.rules import (
+    ChangesRelevantCode,
+    ChangesRelevantFiles,
+    CommitMentionedInReference,
+    CrossReferencedBug,
+    CrossReferencedGh,
+    GHSecurityAdvInMessage,
+    ReferencesGhIssue,
+    VulnIdInLinkedIssue,
+    VulnIdInMessage,
+    apply_rules,
+)
+from util.lsh import get_encoded_minhash
+
+# from datamodel.commit_features import CommitWithFeatures
+
+MOCK_CVE_ID = "CVE-2020-26258"
+
+
+def get_msg(text, limit_length: Optional[int] = None) -> str:
+    return text[:limit_length] if limit_length else text
+
+
+@pytest.fixture
+def candidates():
+    return [
+        # Should match: VulnIdInMessage, ReferencesGhIssue
+        Commit(
+            repository="repo1",
+            commit_id="1234567890",
+            message=f"Blah blah blah fixes {MOCK_CVE_ID} and a few other issues",
+            ghissue_refs={"example": ""},
+            changed_files={"foo/bar/otherthing.xml", "pom.xml"},
+            cve_refs=[f"{MOCK_CVE_ID}"],
+            minhash=get_encoded_minhash(
+                get_msg(
+                    f"Blah blah blah fixes {MOCK_CVE_ID} and a few other issues",
+                    50,
+                )
+            ),
+        ),
+        Commit(
+            repository="repo2",
+            commit_id="2234567890",
+            message="",
+            minhash=get_encoded_minhash(get_msg("")),
+        ),
+        # Should match: VulnIdInMessage, ReferencesGhIssue
+        Commit(
+            repository="repo3",
+            commit_id="3234567890",
+            message=f"Another commit that fixes {MOCK_CVE_ID}",
+            ghissue_refs={"example": ""},
+            cve_refs=[f"{MOCK_CVE_ID}"],
+            minhash=get_encoded_minhash(
+                get_msg(f"Another commit that fixes {MOCK_CVE_ID}", 50)
+            ),
+        ),
+        # Should match: SecurityKeywordsInMsg
+        Commit(
+            repository="repo4",
+            commit_id="4234567890",
+            message="Endless loop causes DoS vulnerability",
+            changed_files={"foo/bar/otherthing.xml", "pom.xml"},
+            minhash=get_encoded_minhash(
+                get_msg("Endless loop causes DoS vulnerability", 50)
+            ),
+        ),
+        # Should match: AdvKeywordsInFiles, SecurityKeywordsInMsg, CommitMentionedInReference
+        Commit(
+            repository="repo5",
+            commit_id="7532d2fb0d6081a12c2a48ec854a81a8b718be62",
+            message="Insecure deserialization",
+            changed_files={
+                "core/src/main/java/org/apache/cxf/workqueue/AutomaticWorkQueueImpl.java"
+            },
+            minhash=get_encoded_minhash(get_msg("Insecure deserialization", 50)),
+        ),
+        # TODO: Not matched by existing tests: GHSecurityAdvInMessage, ReferencesBug, ChangesRelevantCode, TwinMentionedInAdv, VulnIdInLinkedIssue, SecurityKeywordInLinkedGhIssue, SecurityKeywordInLinkedBug, CrossReferencedBug, CrossReferencedGh, CommitHasTwins, ChangesRelevantFiles, CommitMentionedInAdv, RelevantWordsInMessage
+    ]
+
+
+@pytest.fixture
+def advisory_record():
+    return AdvisoryRecord(
+        cve_id=f"{MOCK_CVE_ID}",
+        repository_url="https://github.com/apache/struts",
+        published_timestamp=1607532756,
+        references={
+            "https://reference.to/some/commit/7532d2fb0d60": 1,
+        },
+        keywords=["AutomaticWorkQueueImpl"],
+        # paths=["pom.xml"],
+    )
+
+
+def test_apply_phase_1_rules(candidates: List[Commit], advisory_record: AdvisoryRecord):
+    annotated_candidates = apply_rules(candidates, advisory_record, rules=["phase_1"])
+
+    # Repo 5: Should match: AdvKeywordsInFiles, SecurityKeywordsInMsg, CommitMentionedInReference
+    assert len(annotated_candidates[0].matched_rules) == 3
+
+    matched_rules_names = [item["id"] for item in annotated_candidates[0].matched_rules]
+    assert "ADV_KEYWORDS_IN_FILES" in matched_rules_names
+    assert "COMMIT_IN_REFERENCE" in matched_rules_names
+    assert "SEC_KEYWORDS_IN_MESSAGE" in matched_rules_names
+
+    # Repo 1: Should match: VulnIdInMessage, ReferencesGhIssue
+    assert len(annotated_candidates[1].matched_rules) == 2
+
+    matched_rules_names = [item["id"] for item in annotated_candidates[1].matched_rules]
+    assert "VULN_ID_IN_MESSAGE" in matched_rules_names
+    assert "GITHUB_ISSUE_IN_MESSAGE" in matched_rules_names
+
+    # Repo 3: Should match: VulnIdInMessage, ReferencesGhIssue
+    assert len(annotated_candidates[2].matched_rules) == 2
+
+    matched_rules_names = [item["id"] for item in annotated_candidates[2].matched_rules]
+    assert "VULN_ID_IN_MESSAGE" in matched_rules_names
+    assert "GITHUB_ISSUE_IN_MESSAGE" in matched_rules_names
+
+    # Repo 4: Should match: SecurityKeywordsInMsg
+    assert len(annotated_candidates[3].matched_rules) == 1
+
+    matched_rules_names = [item["id"] for item in annotated_candidates[3].matched_rules]
+    assert "SEC_KEYWORDS_IN_MESSAGE" in matched_rules_names
+
+    # Repo 2: Matches nothing
+    assert len(annotated_candidates[4].matched_rules) == 0
