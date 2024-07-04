485cf83b1d0a7167e14f453e65f6c50c0d2709c8
I748376 <laura.schauer@sap.com>
2024-07-04 13:00:25 +0000

    updates tests to take new version tags into account

--- prospector/git/git_test.py
+++ prospector/git/git_test.py
@@ -43,7 +43,7 @@ def test_get_tags_for_commit(repository: Git):
     if commit is not None:
         tags = commit.find_tags()
         print(tags)
-        assert len(tags) == 106
+        assert len(tags) >= 106
         assert "10.2" in tags and "11.3" in tags and "9.4" in tags
 
 
