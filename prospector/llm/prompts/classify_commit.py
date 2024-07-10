aded174f4cc32fa70f9c2a99b5dc67c36345919d
I748376 <laura.schauer@sap.com>
2024-07-10 12:46:56 +0000

    adds commit classification method to llm_service

--- prospector/llm/prompts/classify_commit.py
+++ prospector/llm/prompts/classify_commit.py
@@ -2,8 +2,8 @@ from langchain.prompts import PromptTemplate
 
 zero_shot = PromptTemplate.from_template(
     """Is the following commit security relevant or not?
-Please provide the output as a boolean value: ```ANSWER:```
-If it is security relevant just answer ```ANSWER:True``` otherwise answer ```ANSWER:False```.
+Please provide the output as a boolean value, either True or False.
+If it is security relevant just answer True otherwise answer False. Do not return anything else.
 
 To provide you with some context, the name of the repository is: {repository_name}, and the
 commit message is: {commit_message}.
@@ -12,5 +12,5 @@ Finally, here is the diff of the commit:
 {diff}\n
 
 
-```ANSWER: ```\n"""
+Your answer:\n"""
 )
