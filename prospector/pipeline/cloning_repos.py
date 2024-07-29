f727ae5e1a44b84823155e9f644dc1bcfe916382
I748376 <laura.schauer@sap.com>
2024-07-29 07:19:41 +0000

    adds script to trigger repository cloning for CVEs

--- prospector/pipeline/cloning_repos.py
+++ prospector/pipeline/cloning_repos.py
@@ -11,7 +11,19 @@ dataset = load_dataset(
 for cve_record in dataset:
     urls.add(cve_record[1])
 
-urls = list(urls)[:10]
+# urls = list(urls)
+# urls = [
+#     "https://github.com/Turistforeningen/node-im-resize",  # CVE-2019-10787
+#     "https://github.com/remy/undefsafe",  # CVE-2019-10795
+#     "https://github.com/Froxlor/Froxlor",  # CVE-2020-10236
+#     "https://github.com/jnunemaker/crack",
+#     "https://github.com/django-tastypie/django-tastypie",
+#     "https://github.com/pyinstaller/pyinstaller",
+#     "https://github.com/rails/rails-html-sanitizer",
+#     "https://github.com/scipy/scipy",
+#     "https://github.com/parcel-bundler/parcel",
+#     "https://github.com/javamelody/javamelody",
+# ]
 
 print(f"Retrieved {len(urls)} distinct repositories from the dataset.")
 
@@ -21,6 +33,7 @@ results = clone_repo_multiple(
     output_folder="/home/i748376/data/gitcache",
     skip_existing=False,
     shallow=False,
+    concurrent=1,
 )
 
 print("Cloning completed. Results: ", results)
