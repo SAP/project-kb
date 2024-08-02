2d9ea2cae34e79c941a732aee46435d0b4d8a23a
I748376 <laura.schauer@sap.com>
2024-08-02 13:58:21 +0000

    moves cloning repositories to fill gitcache into evaluation folder

---
+++ prospector/evaluation/cloning_repos.py
@@ -0,0 +1,30 @@
+from evaluation.utils import load_dataset
+from git.git import clone_repo_multiple
+
+
+# Get the URLs from d63.csv -> set
+urls = set()
+dataset = load_dataset(
+    "/home/i748376/prospector/project-kb/prospector/evaluation/data/input/d63.csv"
+)
+
+for cve_record in dataset:
+    urls.add(cve_record[1])
+
+urls = list(urls)
+urls = [
+    "https://github.com/hueniverse/undefsafe",
+]
+
+print(f"Retrieved {len(urls)} distinct repositories from the dataset.")
+
+# Call clone_repo_multiple() on this set
+results = clone_repo_multiple(
+    urls,
+    output_folder="/home/i748376/data/gitcache",
+    skip_existing=False,
+    shallow=False,
+    concurrent=1,
+)
+
+print("Cloning completed. Results: ", results)
