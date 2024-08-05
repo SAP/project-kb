62dcee1e4c24a29f43f955aaaf3f162de503ec79
I748376 <laura.schauer@sap.com>
2024-08-05 15:11:53 +0000

    [DOC] cleans up utils file

--- prospector/evaluation/utils.py
+++ prospector/evaluation/utils.py
@@ -1,10 +1,7 @@
 import csv
 import json
-import re
 
 from omegaconf import OmegaConf
-from datamodel.advisory import build_advisory_record
-from urllib.parse import urlparse
 from log.logger import create_logger
 
 config = OmegaConf.load("evaluation/config.yaml")
@@ -16,20 +13,9 @@ INPUT_DATA_PATH = config.input_data_path
 # Select the folder depending whether LLMs are used or not
 PROSPECTOR_REPORT_PATH = config.prospector_reports_path
 ANALYSIS_RESULTS_PATH = config.analysis_results_path
-
-VULN = ["version", "through", "versions"]
-
-FIXED = [
-    "before",
-    "before release",
-    "before version",
-    "prior to",
-    "upgrade to",
-    "fixed in",
-    "fixed in version",
-    "fixed in release",
-    "to version",
-]
+# Comparison dirs
+COMPARE_DIRECTORY_1 = config.compare_directory1
+COMPARE_DIRECTORY_2 = config.compare_directory2
 
 
 def load_dataset(path: str):
@@ -45,72 +31,9 @@ def load_json_file(path: str) -> dict:
         return json.load(f)
 
 
-def is_real_version(text: str):
-    return bool(re.match(r"\d+\.(?:\d+\.*)*\d", text))
-
-
-def get_version_spacy(text: str, nlp):
-    """This function extracts vulnerable and fixed version numbers from a given text using spaCy."""
-    doc = nlp(text)
-    # relevant_sentences = {}
-    # relevant_sentence = ""
-    fixed_version = ""
-    vulnerable_version = ""
-    for i in range(len(doc))[1:]:
-        if is_real_version(doc[i].text):
-            if doc[i - 1].text in FIXED:
-                # relevant_sentence = doc[: i + 1]
-                fixed_version = doc[i].text
-            elif (doc[i - 2].text + " " + doc[i - 1].text) in FIXED:
-                # relevant_sentence = doc[: i + 1]
-                fixed_version = doc[i].text
-            elif (
-                doc[i - 3].text + " " + doc[i - 2].text + " " + doc[i - 1].text
-            ) in FIXED:
-                # relevant_sentence = doc[: i + 1]
-                fixed_version = doc[i].text
-            else:
-                # relevant_sentence = doc[: i + 1]
-                vulnerable_version = doc[i].text
-    return vulnerable_version, fixed_version
-
-
-def check_advisory(cve, repository=None, nlp=None):
-    """This function checks the advisory for a given CVE and attempts to extract version information."""
-    advisory = build_advisory_record(
-        cve, nvd_rest_endpoint="http://localhost:8000/nvd/vulnerabilities/"
-    )
-    references = [urlparse(r).netloc for r in advisory.references]
-    return references
-    vuln = "None"
-    if len(advisory.versions.get("affected")):
-        vuln = advisory.versions.get("affected")[-1]
-
-    fixed = "None"
-    if len(advisory.versions.get("fixed")):
-        fixed = advisory.versions.get("fixed")[-1]
-
-    vuln2, fixed2 = get_version_spacy(advisory.description, nlp)
-    res = [advisory.cve_id, advisory.description]
-    if fixed == fixed2 and vuln == vuln2:
-        res.append(f"{vuln}:{fixed}")
-    if fixed == "None" and fixed2 != "":
-        res.append(f"{vuln}:{fixed2}")
-    if vuln == "None" and vuln2 != "":
-        res.append(f"{vuln2}:{fixed}")
-    if fixed != fixed2 and fixed2 != "" and fixed != "None":
-        res.append(f"{vuln}:{fixed}")
-        res.append(f"{vuln}:{fixed2}")
-
-    if len(res) > 2:
-        res.append("***************************************")
-        print(advisory.cve_id)
-        return res
-    else:
-        res.append(f"{vuln}:{fixed}")
-        res.append("***************************************")
-        print(advisory.cve_id)
-        return res
+def save_dict_to_json(dictionary: dict, path: str):
+    with open(path, "w") as file:
+        return json.dump(dictionary, file, indent=4)
 
 
 def update_summary_execution_table(
@@ -168,11 +91,3 @@ def update_summary_execution_table(
             file.writelines(table_lines)
 
     print(f"Updated latex table at {filepath}")
-
-
-def update_false_positives(itm):
-    with open(f"{ANALYSIS_RESULTS_PATH}false_postive.txt", "a") as file:
-        writer = csv.writer(file)
-        writer.writerow(
-            [f"{itm[0]};{itm[1]};{itm[2]};{itm[3]};{itm[4]};{itm[5]}"]
-        )
