cc0b3e88a75a78eafbdeaa8e3646675adf898f3a
I748376 <laura.schauer@sap.com>
2024-08-29 08:55:12 +0000

    adjusts import paths after renaming folder to pipeline

--- prospector/service/api/routers/home.py
+++ prospector/service/api/routers/home.py
@@ -1,15 +1,9 @@
-import time
-
-from api.rq_utils import queue, get_all_jobs
-import redis
-from fastapi import APIRouter, FastAPI, Request
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from rq import Connection, Queue
-from rq.job import Job
-from starlette.responses import RedirectResponse
 
 from util.config_parser import parse_config_file
+from service.api.routers.jobs import connect_to_db
 
 # from core.report import generate_report
 
@@ -26,7 +20,8 @@ redis_url = config.redis_url
 # endpoint for monitoring all job status
 @router.get("/home", response_class=HTMLResponse)
 async def home(request: Request):
-    joblist = get_all_jobs()
+    db = connect_to_db()
+    joblist = db.get_all_jobs()
     return templates.TemplateResponse(
         "index.html", {"request": request, "joblist": joblist}
     )
